c8f97d5a28fc900405bb301c89c80360effe05d3
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-06-10 15:04:10 -0700

    set and sample figures

--- analysis/03-raw-summary-figs.R
+++ analysis/03-raw-summary-figs.R
@@ -6,38 +6,31 @@ library(tidyverse)
 
 # load data ---------------------------------------------------------------
 final <- readRDS("data-generated/dogfishs_allsets_allspecies_counts.rds")
-final <- final |> mutate(cat2 = paste0(category, hook_desc))
+samples <- readRDS("data-raw/dogfish_samples.rds")
 
-#drop comp j hook in 2023
 jhook <- final |> filter(year == 2023 & cat2 == "comp-dogJ-HOOK")
 final <- final |> filter(!fishing_event_id %in% (jhook$fishing_event_id))
-df <- filter(final, species_code == "044")
-df <- df |> mutate(cpue = catch_count / (lglsp_hook_count * soak))
+final <- filter(final, species_code == "044")
+
 
 # Plots - set level summary plots -----------------------------------------
-
-df |>
+final |>
   ggplot() +
   geom_jitter(aes(year, soak, colour = cat2)) +
-  theme_classic()
+  theme_classic() + scale_colour_viridis_d(option = "mako")
 ggsave("Figures/soak-time-variability.png", width = 4, height = 4)
 
-df |>
+final |>
   ggplot() +
   geom_jitter(aes(year, julian, colour = cat2)) +
-  theme_classic()
+  theme_classic() + scale_colour_viridis_d()
 ggsave("Figures/julian-variability.png", width = 4, height = 4)
-
-# ggplot(df, aes(grouping_depth_id, catch_count, group = year, colour = year)) +
-#   geom_jitter() +
-#   geom_line() +
-#   facet_wrap(~grouping_spatial_id)
 
 final |>
   ggplot() +
   geom_jitter(aes(grouping_depth_id, catch_count, group = cat2, colour = cat2)) +
   facet_wrap(~year) + theme_classic() + scale_colour_viridis_d(option = "mako")
-ggsave("Figures/raw-catch-count-by-survey-year.png", width = 4, height = 4)
+ggsave("Figures/raw-catch-count-by-survey-depth.png", width = 4, height = 4)
 
 final |>
   group_by(cat2) |>
@@ -58,33 +51,33 @@ ggsave("Figures/dog-survey-by-site.png", width = 4, height = 4)
 #   geom_jitter() +
 #   facet_wrap(~year, scales = "free_y")
 
-df |>
+final |>
   group_by(grouping_depth_id, year, hooksize_desc) |>
   drop_na(soak) |>
   reframe(sum = sum(catch_count) / sum(lglsp_hook_count * soak)) |>
   ggplot(aes(grouping_depth_id, sum, group = hooksize_desc, colour = hooksize_desc)) +
   geom_point() +
   geom_line() +
-  facet_wrap(~year, scales = "free_y")
+  facet_wrap(~year, scales = "free_y") + theme_classic() + scale_colour_viridis_d(option = "mako")
 
-# d |>
-#   filter(species_code == "044") |>
-#   filter(specimen_sex_code %in% c(1, 2)) |>
-#   ggplot() +
-#   geom_boxplot(aes((grouping_depth_id), total_length,
-#     colour = as.factor(hooksize_desc)
-#   )) +
-#   # geom_boxplot(position = position_dodge(1)) +
-#   facet_wrap(~ specimen_sex_code + year, nrow = 2) +
-#   theme_classic()
 
-# d |>
-#   filter(species_code == "044") |>
-#   filter(specimen_sex_code %in% c(1, 2)) |>
-#   ggplot() +
-#   geom_boxplot(aes(as.factor(year), total_length,
-#     colour = as.factor(hooksize_desc)
-#   )) +
-#   # geom_boxplot(position = position_dodge(1)) +
-#   facet_wrap(~ specimen_sex_code + grouping_depth_id, nrow = 2) +
-#   theme_classic()
+
+# Plots - sample level summary plots --------------------------------------
+
+samples |> group_by(year, category) |>
+  ggplot() +
+  geom_jitter(aes(year, total_length, colour = category)) + theme_classic()
+
+samples |> group_by(year, category) |>
+  ggplot() +
+  geom_jitter(aes(year, total_length, colour = category)) +
+  facet_wrap(~specimen_sex_code)
+ggsave("Figures/length-variability.png", width = 4, height = 4)
+
+samples |>
+  filter(year %in% c(2005, 2008, 2011, 2014, 2019) & category == "dog") |>
+  filter(specimen_sex_code %in% c(1,2)) |>
+  ggplot() +
+  geom_jitter(aes(year, total_length), alpha = 0.5) +
+  facet_wrap(~specimen_sex_code) + theme_classic()
+ggsave("Figures/length-variability-dogsurv.png", width = 4, height = 4)
