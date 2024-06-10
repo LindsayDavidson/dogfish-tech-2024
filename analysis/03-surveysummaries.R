df9d028b83430564ac25aab522c64941e2a4419f
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-06-10 15:05:59 -0700

    cleaned final df, julian date

--- analysis/03-surveysummaries.R
+++ analysis/03-surveysummaries.R
@@ -1,10 +1,10 @@
 #Calculate the number of sets and the number of hooks for each year/survey type
 # Code for creating one database of all Dogfish surveys including comparisons, j hooks, and dogfish surveys
 # Note
-# SURVEY_SERIES_ID == 48) #2004, 2019, 2022, 2023 survey since it was 2 different sets with different hooks
-# SURVEY_SERIES_ID == 93) # 2005 onwards dogfish survey
-# SURVEY_SERIES_ID == 76) # 1986 onwards dogfish survey DROP THIS ONE
-# SURVEY_SERIES_ID == 92) # 1986, 1989 survey
+# SURVEY_SERIES_ID == 48) #comp work 2004, 2019, 2022, 2023
+# SURVEY_SERIES_ID == 93) #circle hook dog surveys 2005 onwards (2005, 2008, 2011, 2014, 2019)
+# SURVEY_SERIES_ID == 76) #all jhook and circle hook dog surveys 1986 onwards does not include 2004 (1986, 1989, 2005, 2008, 2011, 2014, 2019)
+# SURVEY_SERIES_ID == 92) #j hook dog surveys 1986, 1989 only
 
 # yelloweye rockfish were not sampled in earlier years. 1986/1989 maybe not 2004?
 # 2004 comparison work had two gear types per set
@@ -12,26 +12,37 @@
 # 2022 comparison work has two gear types per set
 # 2023 comparison work has two gear types per set  and was completed during the HBLL and DOG survey
 
+# library -----------------------------------------------------------------
+
+library(ggplot2)
+library(tidyverse)
+
 # load data ---------------------------------------------------------------
-#cleaned data from 01-pull_data.R
-sets <- readRDS("data/generated/sets_cleaned2.rds")
-count <- readRDS("data/raw/dogfish_counts.rds")
-sets_df <- readRDS("data/generated/dogfishs_allsets_allspecies_counts.rds")
-#saveRDS(final, "data/raw/dogfishs_allsets_allsamples.rds")
 
+final <- readRDS("data-generated/dogfishs_allsets_allspecies_counts.rds")
+samples <- readRDS("data-raw/dogfish_samples.rds")
 
+#take out the 2023 J hook sets as that was for the yelloweye comparative work, not dogfish
+jhook <- final |> filter(year == 2023 & cat2 == "comp-dogJ-HOOK")
+final <- final |> filter(!fishing_event_id %in% (jhook$fishing_event_id))
+final <- filter(final, species_code == "044")
+
+# sets <- readRDS("data-generated/sets_cleaned2.rds")
+# count <- readRDS("data-raw/dogfish_counts.rds")
+# sets_df <- readRDS("data-generated/dogfishs_allsets_allspecies_counts.rds")
+
+final |> group_by(survey_series_id) |> distinct(year, .keep_all = TRUE) |>
+  reframe(count = n())
+final |> group_by(survey_series_id, year) |> distinct() |>
+  reframe()
 
 # number of sets dropped each year ----------------------------------------
 
-d <- filter(sets, survey_series_id == 48)
+d <- filter(final, survey_series_id == 48)
 sort(unique(d$year))
 #d <- filter(sets, survey_series_id == 93)
 #d <- filter(sets, survey_series_id == 76)
 #unique(d$year)
-
-
-#take out the J hook sets as that was for the yelloweye comparative work, not dogfish
-jhook <- d |> filter(hook_desc == "J-HOOK")
 
 d |>
   filter(fishing_event_id %in% jhook$fishing_event_id) |>
