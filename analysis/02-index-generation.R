acc3bed6e9a679aea9a7c557b05dcf6eecb2befb
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-06-18 16:21:08 -0700

    index

--- analysis/02-index-generation.R
+++ analysis/02-index-generation.R
@@ -1,15 +1,19 @@
-
 # notes -------------------------------------------------------------------
 
-#2004 is missing deployment times and therefore I can't caluculate soak time
-#soak times were between 1.5-3 hours
+# 2004 is missing deployment times and therefore I can't caluculate soak time
+# soak times were between 1.5-3 hours
 
+
+# params ------------------------------------------------------------------
+
+bccrs <- 32609
 
 
 # library -----------------------------------------------------------------
 
 library(ggplot2)
 library(tidyverse)
+library(sdmTMB)
 
 # load hbll data ---------------------------------------------------------------
 
@@ -18,7 +22,7 @@ hbll <- hbll |> dplyr::select(survey_desc, year, fishing_event_id, latitude, lon
 
 hbll <- hbll |>
   mutate(
-    #time_retrieved = as.Date(time_retrieved, format = "%Y-%m-%d h:m:s"),
+    # time_retrieved = as.Date(time_retrieved, format = "%Y-%m-%d h:m:s"),
     julian = lubridate::yday(time_deployed),
     retrievehr = lubridate::hour(time_retrieved),
     retrievemin = lubridate::minute(time_retrieved),
@@ -50,11 +54,12 @@ hbll |>
 final <- readRDS("data-generated/dogfish_sets_cleaned.rds") |>
   filter(species_code == "044")
 unique(final$year)
-final <- final |> dplyr::select(
-  survey_series_desc, year, survey, fishing_event_id, latitude, longitude, depth_m, lglsp_hook_count, catch_count,
-  julian, soak
-) |>
-  rename("survey_desc" = "survey_series_desc",  "hook_count" = "lglsp_hook_count", "survey_abbrev" = "survey")
+final <- final |>
+  dplyr::select(
+    survey_series_desc, year, survey, fishing_event_id, latitude, longitude, depth_m, lglsp_hook_count, catch_count,
+    julian, soak
+  ) |>
+  rename("survey_desc" = "survey_series_desc", "hook_count" = "lglsp_hook_count", "survey_abbrev" = "survey")
 final <- final |>
   mutate(soak = ifelse(year %in% c(2004, 2005), 2, soak))
 
@@ -73,6 +78,13 @@ final |>
 
 d <- bind_rows(final, hbll)
 
+# convert to UTMs
+d <- add_utm_columns(d,
+  ll_names = c("longitude", "latitude"),
+  utm_names = c("UTM.lon", "UTM.lat"),
+  utm_crs = bccrs
+) |>
+  mutate(UTM.lon.m = UTM.lon * 1000, UTM.lat.m = UTM.lat * 1000)
 
 # summary plots -----------------------------------------------------------
 
@@ -87,7 +99,19 @@ d |>
 
 d |>
   ggplot() +
-  geom_point(aes(longitude, latitude))
+  geom_point(aes(longitude, latitude, colour = survey_abbrev)) +
+  facet_wrap(~year)
+
+d |>
+  ggplot() +
+  geom_point(aes(UTM.lon, UTM.lat, colour = survey_abbrev)) +
+  facet_wrap(~year)
+
+d |>
+  ggplot() +
+  geom_point(aes(longitude, latitude, colour = catch_count, size = catch_count)) +
+  facet_wrap(~year) +
+  scale_colour_viridis_c(trans = "sqrt")
 
 
 d |>
@@ -120,6 +144,7 @@ d |>
 
 hbll_ins <- gfplot::hbll_inside_s_grid
 plot(hbll_ins$grid$X, hbll_ins$grid$Y)
+points(d$longitude, d$latitude, col = "red")
 range(hbll_ins$grid)
 # needs depth
 range(hbll_ins$grid$X)
@@ -137,12 +162,70 @@ bdepths <- marmap::get.depth(b, hbll_ins$grid[, c("X", "Y")], locator = FALSE) |
 
 bdepths[duplicated(bdepths), ] # just checking
 
+pred <- filter(bdepths, depth < 0)
+plot(hbll_ins$grid$X, hbll_ins$grid$Y)
+points(pred$lon, pred$lat, col = "red")
 
+pred <- add_utm_columns(pred,ll_names = c("lon", "lat"),
+                     utm_names = c("UTM.lon", "UTM.lat"),
+                     utm_crs = bccrs) |>
+  mutate(UTM.lon.m = UTM.lon * 1000, UTM.lat.m = UTM.lat * 1000) |>
+  mutate(log_botdepth = log(bot_depth))
 
 
 # mesh  -------------------------------------------------------------------
 
+mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 5)
+plot(mesh)
+mesh$mesh$n
+
+ggplot() +
+  inlabru::gg(mesh$mesh) +
+  geom_point(data = d, aes(UTM.lon, UTM.lat), size = 0.5, alpha = 0.7, pch = 21) +
+  xlab("UTM (km)") +
+  ylab("UTM (km)") +
+  coord_fixed()
+ggsave("figs/mesh.pdf", width = 6, height = 6)
 
 
 
 # model -------------------------------------------------------------------
+
+
+fit <- sdmTMB(
+  formula = catch_count ~ poly(log_botdepth,2),
+  data = d,
+  time = "year",
+  offset = "offset",
+  mesh = mesh,
+  spatial = "on",
+  spatiotemporal = "rw",
+  family = nbinom2(),
+  silent = FALSE,
+  share_range = FALSE,
+  priors = sdmTMBpriors(
+    matern_s = pc_matern(range_gt = 250, sigma_lt = 2),
+    matern_st = pc_matern(range_gt = 250, sigma_lt = 2)
+  )
+)
+toc()
+
+saveRDS(fit, file = "output/fit-sog-hblldog.rds")
+fit <- readRDS("output/fit-sog-hblldog.rds")
+
+years <- seq(min(df$year), 2022, 1)
+grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
+
+sanity(fit)
+
+pred<- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
+index <- get_index(pred, bias_correct = TRUE)
+
+years <- unique(index$year)
+ggplot(index, aes(year, est/10000)) +
+  geom_line(col = "#8D9999") +
+  geom_point(col = "#8D9999") +
+  geom_ribbon(aes(ymin = lwr/10000, ymax = upr/10000), alpha = 0.4, fill = "#8D9999") +
+  theme_classic() +
+  scale_x_continuous(breaks = c(years))
+
