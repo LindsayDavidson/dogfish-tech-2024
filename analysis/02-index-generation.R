b6c2dccb26b94576e494d6aa15b4ff84afe3bc9b
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-06-19 11:47:44 -0700

    pulled out wrangling, prediction grid, summary plots

--- analysis/02-index-generation.R
+++ analysis/02-index-generation.R
@@ -9,169 +9,35 @@
 bccrs <- 32609
 
 
+# map ---------------------------------------------------------------------
+sf::sf_use_s2(FALSE)
+map_data <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf")
+coast <- sf::st_crop(
+  map_data,
+  c(xmin = -175, ymin = 20, xmax = -115, ymax = 70)
+)
+
+coast_proj <- sf::st_transform(coast, crs = 32612)
+
+
 # library -----------------------------------------------------------------
 
 library(ggplot2)
 library(tidyverse)
 library(sdmTMB)
 
-# load hbll data ---------------------------------------------------------------
 
-hbll <- readRDS("data-raw/hbllsets.rds")
-hbll <- hbll |> dplyr::select(survey_desc, year, fishing_event_id, latitude, longitude, depth_m, hook_count, catch_count, survey_abbrev, time_deployed, time_retrieved)
+# pull data ---------------------------------------------------------------
 
-hbll <- hbll |>
-  mutate(
-    # time_retrieved = as.Date(time_retrieved, format = "%Y-%m-%d h:m:s"),
-    julian = lubridate::yday(time_deployed),
-    retrievehr = lubridate::hour(time_retrieved),
-    retrievemin = lubridate::minute(time_retrieved),
-    deployhr = lubridate::hour(time_deployed),
-    deploymin = lubridate::minute(time_deployed)
-  ) |>
-  filter(is.na(retrievehr) == FALSE) |>
-  filter(survey_abbrev == "HBLL INS S") |>
-  mutate(
-    hr_diff = (retrievehr - deployhr) * 60,
-    min_diff = abs(retrievemin - deploymin),
-    soak = (hr_diff + min_diff) / 60
-  ) |>
-  dplyr::select(!c(retrievehr, retrievemin, deployhr, deploymin, hr_diff, min_diff, time_deployed, time_retrieved))
-
-hbll$offset <- log(hbll$hook_count * hbll$soak)
-hbll$log_botdepth <- log(hbll$depth_m)
-hbll$survey_abbrev <- "hbll"
-hbll <- filter(hbll, !(latitude < 48.5 & longitude < -123))
-hbll <- filter(hbll, !(latitude < 48.75 & longitude < -124.25))
-hbll |>
-  ggplot() +
-  geom_point(aes(longitude, latitude))
+d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds")
+d <- filter(d, year != 2004)
 
 
+# grid --------------------------------------------------------------------
 
-# load dogfish data -------------------------------------------------------
-
-final <- readRDS("data-generated/dogfish_sets_cleaned.rds") |>
-  filter(species_code == "044")
-unique(final$year)
-final <- final |>
-  dplyr::select(
-    survey_series_desc, year, survey, fishing_event_id, latitude, longitude, depth_m, lglsp_hook_count, catch_count,
-    julian, soak
-  ) |>
-  rename("survey_desc" = "survey_series_desc", "hook_count" = "lglsp_hook_count", "survey_abbrev" = "survey")
-final <- final |>
-  mutate(soak = ifelse(year %in% c(2004, 2005), 2, soak))
-
-final <- final |> filter(!is.na(soak))
-final <- final |> filter(!is.na(julian))
-final$offset <- log(final$hook_count * final$soak)
-final$log_botdepth <- log(final$depth_m)
-range(final$depth_m)
-glimpse(final)
-unique(final$survey_abbrev)
-
-final |>
-  group_by(survey_abbrev, year) |>
-  distinct() |>
-  reframe()
-
-d <- bind_rows(final, hbll)
-
-# convert to UTMs
-d <- add_utm_columns(d,
-  ll_names = c("longitude", "latitude"),
-  utm_names = c("UTM.lon", "UTM.lat"),
-  utm_crs = bccrs
-) |>
-  mutate(UTM.lon.m = UTM.lon * 1000, UTM.lat.m = UTM.lat * 1000)
-
-# summary plots -----------------------------------------------------------
-
-d |>
-  group_by(survey_abbrev, year) |>
-  mutate(
-    catch_count_sum = sum(catch_count),
-    cpue = sum(catch_count) / sum(hook_count)
-  ) |>
-  ggplot() +
-  geom_point(aes(year, cpue, colour = survey_abbrev))
-
-d |>
-  ggplot() +
-  geom_point(aes(longitude, latitude, colour = survey_abbrev)) +
-  facet_wrap(~year)
-
-d |>
-  ggplot() +
-  geom_point(aes(UTM.lon, UTM.lat, colour = survey_abbrev)) +
-  facet_wrap(~year)
-
-d |>
-  ggplot() +
-  geom_point(aes(longitude, latitude, colour = catch_count, size = catch_count)) +
-  facet_wrap(~year) +
-  scale_colour_viridis_c(trans = "sqrt")
-
-
-d |>
-  group_by(survey_abbrev, year) |>
-  ggplot() +
-  geom_jitter(aes(year, log(catch_count), colour = survey_abbrev))
-
-
-d |>
-  group_by(survey_abbrev, year) |>
-  ggplot() +
-  geom_jitter(aes(year, hook_count, colour = survey_abbrev))
-
-
-d |>
-  group_by(survey_abbrev, year) |>
-  ggplot() +
-  geom_jitter(aes(year, julian, colour = survey_abbrev))
-
-
-d |>
-  group_by(survey_abbrev, year) |>
-  ggplot() +
-  geom_jitter(aes(year, soak, colour = survey_abbrev))
-
-
-
-
-# prediction grid from gfplot ---------------------------------------------
-
-hbll_ins <- gfplot::hbll_inside_s_grid
-plot(hbll_ins$grid$X, hbll_ins$grid$Y)
-points(d$longitude, d$latitude, col = "red")
-range(hbll_ins$grid)
-# needs depth
-range(hbll_ins$grid$X)
-range(hbll_ins$grid$Y)
-hbll_ins$grid <- hbll_ins$grid[!duplicated(hbll_ins$grid), ] # just checking
-
-b <- marmap::getNOAA.bathy(lon1 = -126, lon2 = -122, lat1 = 47, lat2 = 51, resolution = 1)
-
-bdepths <- marmap::get.depth(b, hbll_ins$grid[, c("X", "Y")], locator = FALSE) |>
-  mutate(bot_depth = (depth * -1))
-# rename(longitude = lon, latitude = lat) |>
-# filter(bot_depth > 25) |>
-# mutate(logbot_depth = log(bot_depth)) |>
-# inner_join(hbll_ins$grid, by = c("X" = "X", "Y" = "Y"))
-
-bdepths[duplicated(bdepths), ] # just checking
-
-pred <- filter(bdepths, depth < 0)
-plot(hbll_ins$grid$X, hbll_ins$grid$Y)
-points(pred$lon, pred$lat, col = "red")
-
-pred <- add_utm_columns(pred,ll_names = c("lon", "lat"),
-                     utm_names = c("UTM.lon", "UTM.lat"),
-                     utm_crs = bccrs) |>
-  mutate(UTM.lon.m = UTM.lon * 1000, UTM.lat.m = UTM.lat * 1000) |>
-  mutate(log_botdepth = log(bot_depth))
-
+grid <- readRDS("output/prediction-grid-sog.rds")
+grid$survey_abbrev <- "hbll"
+grid$julian <- mean(d$julian)
 
 # mesh  -------------------------------------------------------------------
 
@@ -190,10 +56,10 @@ ggsave("figs/mesh.pdf", width = 6, height = 6)
 
 
 # model -------------------------------------------------------------------
-
+unique(sort(d$year))
 
 fit <- sdmTMB(
-  formula = catch_count ~ poly(log_botdepth,2),
+  formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_abbrev),
   data = d,
   time = "year",
   offset = "offset",
@@ -203,29 +69,36 @@ fit <- sdmTMB(
   family = nbinom2(),
   silent = FALSE,
   share_range = FALSE,
-  priors = sdmTMBpriors(
-    matern_s = pc_matern(range_gt = 250, sigma_lt = 2),
-    matern_st = pc_matern(range_gt = 250, sigma_lt = 2)
-  )
+  extra_time = c(1987, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2006, 2007, 2010, 2012, 2016, 2017, 2020)
 )
-toc()
 
 saveRDS(fit, file = "output/fit-sog-hblldog.rds")
 fit <- readRDS("output/fit-sog-hblldog.rds")
 
-years <- seq(min(df$year), 2022, 1)
+years <- seq(min(d$year), 2023, 1)
+# grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
 grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
 
 sanity(fit)
 
-pred<- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
+pred <- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
 index <- get_index(pred, bias_correct = TRUE)
 
-years <- unique(index$year)
-ggplot(index, aes(year, est/10000)) +
+index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
+yearlabs <- as.list(index |> filter(model == "yrs_surved") |> reframe(year = year))
+yearlabs <- c(1986, 1989, 2005, 2008, 2009, 2011, 2013, 2014, 2015, 2018, 2019, 2021, 2022, 2023)
+
+ggplot(indexf, aes(year, est / 10000)) +
   geom_line(col = "#8D9999") +
   geom_point(col = "#8D9999") +
-  geom_ribbon(aes(ymin = lwr/10000, ymax = upr/10000), alpha = 0.4, fill = "#8D9999") +
+  geom_ribbon(aes(ymin = lwr / 10000, ymax = upr / 10000), alpha = 0.4, fill = "#8D9999") +
   theme_classic() +
   scale_x_continuous(breaks = c(years))
 
+
+ggplot(index, aes(year, est, ymin = lwr, ymax = upr)) +
+  # geom_pointrange(data = filter(index, model == "yrs_interp"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "grey40", alpha = 0.6) +
+  geom_pointrange(data = filter(index, model == "yrs_surved"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "red", alpha = 0.6) +
+  theme_classic() +
+  scale_x_continuous(breaks = c(yearlabs)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
