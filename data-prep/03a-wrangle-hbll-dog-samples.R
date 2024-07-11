61f2c74a6613c324631578d4f27835b1ba112ef1
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-07-11 14:30:49 -0700

    rm old analysis

--- data-prep/03a-wrangle-hbll-dog-samples.R
+++ data-prep/03a-wrangle-hbll-dog-samples.R
@@ -24,7 +24,7 @@ dsamps <- readRDS("data-raw/dogfish_samples_cleaned.rds") |>
   filter(total_length > 0) |>
   mutate(total_length = total_length/10)
 range(dsamps$total_length)
-
+unique(dsamps$survey)
 
 # set data (to remove fishing sets)
 hbll <- readRDS("data-raw/hbllsets.rds")
@@ -83,7 +83,7 @@ dsamps2 <- dsamps |>
   )
 
 samps <- bind_rows(dsamps2, hsamps2)
-
+saveRDS(samps, "output/samps_joined.rds")
 
 
 # summary plot ------------------------------------------------------------
@@ -95,100 +95,3 @@ ggplot(samps, aes(year, length, col = survey_abbrev)) +
   facet_wrap(~sex)
 
 
-
-
-
-
-
-# anova - are the group means different? ----------------------------------
-
-f <- filter(samps, sex == 2)
-mean(f$length[(f$name) == "DOG"])
-mean(f$length[(f$name) == "HBLL INS S"])
-
-aov <- aov(length ~ name, data = filter(samps, sex == 2))
-summary(aov)
-# plot(aov)
-tukey <- TukeyHSD(aov)
-tukey
-
-aov <- aov(length ~ name, data = filter(samps, sex == 1))
-summary(aov)
-# plot(aov)
-tukey <- TukeyHSD(aov)
-tukey
-plot(tukey)
-
-
-# GLM of sex and survey ---------------------------------------------------
-
-f <- filter(samps, sex == 2 & name %in% c("DOG", "HBLL INS S"))
-f <- filter(samps, sex == 2 & name == "DOG")
-f <- filter(samps, sex == 2 & name == "HBLL INS S")
-m <- glm(length ~ name * year, data = f, family = gaussian())
-summary(m)
-
-# Ratio of M:F  -----------------------------------------------------------
-
-# females maturing 77 - 95.5
-# males maturing
-samps
-glimpse(samps)
-
-samps |>
-  drop_na(weight) |>
-  tally()
-samps |> tally()
-115952 - 6984 # lots of NAs for weight
-
-# use maturity DFO of 55
-# these legnths are from a length weight and length matuirty curve calculated in
-# split-index_by_regionandmaturity.R
-test <- samps |>
-  filter(sex %in% c(1, 2)) |>
-  mutate(lengthgroup = ifelse(length >= 77 & length < 95.5 & sex == 2, "maturingf",
-    ifelse(length >= 65.1 & length < 76.7 & sex == 1, "maturingm",
-      ifelse(length >= 95.5 & sex == 2, "mf",
-        ifelse(length >= 76.7 & sex == 1, "mm",
-          ifelse(length < 77 & sex == 2, "imm",
-            ifelse(length < 65.1 & sex == 1, "imm",
-              NA
-            )
-          )
-        )
-      )
-    )
-  )) |>
-  group_by(year, lengthgroup, name) |>
-  mutate(count = n())
-
-ggplot(filter(test, name != "DOGJhooks"), aes(year, count, group = name, colour = name)) +
-  geom_line() +
-  facet_wrap(~lengthgroup, scales = "free") +
-  theme_classic() +
-  geom_point()
-
-
-# Mature female ratio -----------------------------------------------------
-
-test2 <- test |>
-  group_by(year, fishing_event_id, name) |>
-  filter(lengthgroup != "immatures") |>
-  mutate(sumall = n())
-test3 <- test |>
-  group_by(year, name) |>
-  filter(lengthgroup == "mf") |>
-  summarize(summf = n())
-testmm <- test |>
-  group_by(year, name) |>
-  filter(lengthgroup == "mm") |>
-  summarize(summm = n())
-
-final <- inner_join(test3, testmm, by = c("year", "name"))
-final <- final |>
-  mutate(ratio = summf / summm * 100)
-ggplot(final, aes(year, ratio, group = name)) +
-  geom_point() +
-  geom_line() +
-  facet_wrap(~name) +
-  ylab("Ratio (Mature females:Mature Males)")
