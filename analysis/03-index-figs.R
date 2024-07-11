e80904c6f70d558044a21577c997b479bf55cc2d
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-07-11 12:58:09 -0700

    clean up ggplot code

--- analysis/03-index-figs.R
+++ analysis/03-index-figs.R
@@ -1,49 +1,55 @@
-i_hblldog <- readRDS("output/ind-sog-hblldog.rds") #noth n and s hbll and dog
-i_hblldog <- i_hblldog |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
+i_hblldog <- readRDS("output/ind-sog-hblldog.rds")|> mutate(type = "all") #noth n and s hbll and dog
 yearlabs <- as.list(i_hblldog |> filter(model == "yrs_surved") |> reframe(year = year))
 yearlabs <- yearlabs$year
 
-i_hblln <- readRDS("output/ind-sog-hbll-n.rds")
-i_hblln <- i_hblln |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
-yearlabs <- as.list(i_hblln |> filter(model == "yrs_surved") |> reframe(year = year))
-#yearlabs <- yearlabs$year
+i_hblln <- readRDS("output/ind-sog-hbll-n.rds") |> mutate(type = "hblln")
 
-i_hblls<- readRDS("output/ind-sog-hbll-s.rds")
-i_hblls <- i_hblls |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
-yearlabs <- as.list(i_hblls |> filter(model == "yrs_surved") |> reframe(year = year))
-#yearlabs <- yearlabs$year
+i_hblls<- readRDS("output/ind-sog-hbll-s.rds") |> mutate(type = "hblls")
 
-i_hbllsdog <- readRDS("output/ind-sog-dog-hblls.rds") #hbll s and dog
-i_hbllsdog <- i_hbllsdog |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
-yearlabs <- as.list(i_hbllsdog |> filter(model == "yrs_surved") |> reframe(year = year))
-#yearlabs <- yearlabs$year
+i_hbllsdog <- readRDS("output/ind-sog-dog-hblls.rds") |> mutate(type = "hblls_dog") #hbll s and dog
 
+index <- bind_rows(i_hblldog, i_hblln, i_hblls, i_hbllsdog)
+index <- index |>
+  group_by(type) |>
+  mutate(geomean = log(mean(log(index$est)))) |>
+  mutate(est = est / geomean,
+         lwr = lwr / geomean,
+         upr = upr / geomean
+  )
 
 # figure code -------------------------------------------------------------
 
 
-
-ggplot(index, aes(year, est / 10000)) +
-  geom_line(col = "#8D9999") +
-  geom_point(col = "#8D9999") +
-  geom_ribbon(aes(ymin = lwr / 10000, ymax = upr / 10000), alpha = 0.4, fill = "#8D9999") +
+index |>
+  filter(model == "yrs_surved") |>
+  ggplot(aes(year, log(est), group = type, col = type, fill = type)) +
+  geom_line() +
+  geom_point() +
+  geom_ribbon(aes(ymin = log(lwr), ymax = log(upr)), alpha = 0.4) +
   theme_classic() +
   scale_x_continuous(breaks = c(years))
 
-ggplot(index, aes(year, est, ymin = lwr, ymax = upr)) +
-  # geom_pointrange(data = filter(index, model == "yrs_interp"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "grey40", alpha = 0.6) +
-  geom_pointrange(data = filter(index, model == "yrs_surved"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "red", alpha = 0.6) +
+index |>
+  filter(model == "yrs_surved") |>
+  ggplot(aes(year, (est), ymin = (lwr), ymax = (upr), colour = type, fill = type)) +
+  geom_pointrange(aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6) +
+  theme_classic() +
+  scale_x_continuous(breaks = c(yearlabs)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  facet_wrap(~type, scales = "free")
+
+index |>
+  filter(model == "yrs_surved") |>
+  ggplot(aes(year, log(est), ymin = log(lwr), ymax = log(upr), colour = type, fill = type)) +
+  geom_pointrange(aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6) +
   theme_classic() +
   scale_x_continuous(breaks = c(yearlabs)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-x <-
-  ggplot(i_hblldog, aes(year, est, ymin = lwr, ymax = upr)) +
-  # geom_pointrange(data = filter(index, model == "yrs_interp"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "grey40", alpha = 0.6) +
-  geom_pointrange(data = filter(ind, model == "yrs_surved"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "red", alpha = 0.6) +
+index |>
+  filter(model == "yrs_surved") |>
+  ggplot(aes(year, (est), ymin = (lwr), ymax = (upr), colour = type, fill = type)) +
+  geom_pointrange(aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6) +
   theme_classic() +
   scale_x_continuous(breaks = c(yearlabs)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
-
-y <- x + geom_pointrange(data = filter(i_hbllsdog, model == "yrs_surved"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "grey", alpha = 0.6) + theme_classic()
-z <- y + geom_pointrange(data = filter(i_hblln, model == "yrs_surved"), mapping = aes(x = year - 0.25), size = 0.2, pch = 5, colour = "grey", alpha = 0.6) + theme_classic()
