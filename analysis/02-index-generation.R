3c2adbf6ca3edc71ba0349ce7ee8ba22f3f949cb
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-07-17 11:04:16 -0700

    rm 2004, generate index, add month to model

--- analysis/02-index-generation.R
+++ analysis/02-index-generation.R
@@ -28,6 +28,7 @@ coast_proj <- sf::st_transform(coast, crs = bccrs)
 
 # all
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds")
+sort(unique(d$year))
 grid <- readRDS("output/prediction-grid-hbll-n-s-dog.rds") # from convex hull to have deeper depths
 grid$area_km2 <- as.numeric(grid$area_km)
 years <- seq(min(d$year), 2023, 1)
@@ -37,7 +38,26 @@ grid$survey_type <- "hbll"
 grid$julian <- mean(d$julian)
 path <- "output/fit-sog-hblldog.rds"
 pathind <- "output/ind-sog-hblldog.rds"
-extratime <- c(1987, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2006, 2007, 2010, 2012, 2016, 2017, 2020)
+extratime <- c(1987, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2006, 2007, 2017, 2020)
+
+plot(grid$longitude, grid$latitude)
+points(d$longitude, d$latitude, col = "red")
+
+# all without 2004 comp work
+d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |>
+  filter(year != 2004)
+grid <- readRDS("output/prediction-grid-hbll-n-s-dog.rds") # from convex hull to have deeper depths
+grid$area_km2 <- as.numeric(grid$area_km)
+years <- seq(min(d$year), 2023, 1)
+# grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
+grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
+grid$survey_type <- "hbll"
+grid$julian <- mean(d$julian)
+grid$month <- 7
+path <- "output/fit-sog-hblldog_no2004.rds"
+pathind <- "output/ind-sog-hblldog_no2004.rds"
+sort(unique(d$year))
+extratime <- c(1987, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2004, 2006, 2017, 2020)
 
 plot(grid$longitude, grid$latitude)
 points(d$longitude, d$latitude, col = "red")
@@ -122,17 +142,15 @@ points(d$longitude, d$latitude, col = "red")
 # d <- filter(d, !(fishing_event_id %in% rm$fishing_event_id))
 
 ggplot(d, aes(longitude, latitude, colour = survey_abbrev)) +
-  geom_point()
-ggplot(d, aes(longitude, latitude, colour = survey_type)) +
-  geom_point()
+  geom_point() + theme_classic()
 
 d |>
   ggplot( ) +
-  geom_point(aes(year, catch_count, colour = survey_type))
+  geom_jitter(aes(year, catch_count, colour = survey_type, size = catch_count))
 
 d |>
   ggplot( ) +
-  geom_point(aes(catch_count, log_botdepth,  colour = survey_type))
+  geom_point(aes(log_botdepth, catch_count,   colour = survey_type))
 
 d |>
   ggplot( ) +
@@ -157,26 +175,51 @@ range(d$log_botdepth)
 # make this a function so that I can create index for HBLL N, HBLL --------
 
 indexfunc <- function(d) {
-  #mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 5)
-  mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 2) #hbll n only
+  mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 5)
+  #mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 2) #hbll n only
   plot(mesh)
   mesh$mesh$n
 
+
+
   ggplot() +
-    inlabru::gg(mesh$mesh) +
-    geom_point(data = d, aes(UTM.lon, UTM.lat), size = 0.5, alpha = 0.7, pch = 21) +
-    xlab("UTM (km)") +
-    ylab("UTM (km)") +
-    coord_fixed()
-  # ggsave("Figures/mesh.pdf", width = 6, height = 6)
+    geom_point(data = d |> arrange(year), aes(UTM.lon, UTM.lat
+                                                      , colour = year), size = 1,
+               # ), size = 0.25, alpha = 0.25,
+               pch = 16) +
+    inlabru::gg(mesh$mesh,
+                edge.color = "grey60",
+                edge.linewidth = 0.15,
+                # interior = TRUE,
+                # int.color = "blue",
+                int.linewidth = 0.25,
+                exterior = FALSE,
+                # ext.color = "black",
+                ext.linewidth = 0.5) +
+    scale_colour_viridis_c(direction = -1) +
+    labs(colour = "Year") +
+    xlab("UTM (km)") + ylab("UTM (km)") + coord_fixed(expand = FALSE) +
+    theme_classic() +
+    theme(legend.position = "inside", legend.position.inside = c(0.2, 0.25))
+  # ggsave(paste0("figs/trawl-model-mesh-",min_edge,"-", max_edge,".pdf"), width = 6, height = 6)
+  ggsave("Figures/model-mesh.png", width = 6, height = 6)
+
+
+  # ggplot() +
+  #   inlabru::gg(mesh$mesh) +
+  #   geom_point(data = d, aes(UTM.lon, UTM.lat), size = 0.5, alpha = 0.7, pch = 21) +
+  #   xlab("UTM (km)") +
+  #   ylab("UTM (km)") +
+  #   coord_fixed()
+  # # ggsave("Figures/mesh.pdf", width = 6, height = 6)
 
   unique(sort(d$year))
   unique(sort(d$survey_type))
 
   fit <- sdmTMB(
     formula =
-    #catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type),
-    catch_count ~ poly(log_botdepth, 2) , #hbll n model, hbll s model
+    catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type),
+    #catch_count ~ poly(log_botdepth, 2) , #hbll n model, hbll s model
     data = d,
     time = "year",
     offset = "offset",
@@ -195,7 +238,17 @@ indexfunc <- function(d) {
   fit$sd_report
   sanity(fit)
   # fitjul <- update(fit, formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type) + poly(julian, 2))
-  saveRDS(fit, file = path)
+   fitmonth <- update(fit, formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type) + poly(month, 2))
+   sanity(fitmonth)
+   fitmonth$sd_report
+   # saveRDS(fitmonth, file = "output/fit-sog-hblldog-month.rds")
+   fitmonth <- readRDS(file = "output/fit-sog-hblldog-month.rds")
+
+   pred <- predict(fitmonth, grid, return_tmb_object = TRUE, response = TRUE)
+   index <- get_index(pred, bias_correct = TRUE)
+   saveRDS(index, file = "output/ind-sog-hblldog-month.rds")
+
+   saveRDS(fit, file = path)
   # saveRDS(fitjul, file = "output/fit-sog-hblldog-julian.rds")
 
   pred <- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
@@ -205,6 +258,7 @@ indexfunc <- function(d) {
 
   index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
   saveRDS(index, file = pathind)
+
   yearlabs <- as.list(index |> filter(model == "yrs_surved") |> reframe(year = year))
   yearlabs <- yearlabs$year
     #c(1986, 1989, 2005, 2008, 2009, 2011, 2013, 2014, 2015, 2018, 2019, 2021, 2022, 2023)
