46c5c0db851c1ade33cc282c8b46da2bd19950ec
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-07-19 10:46:48 -0700

    new grid, add month,

--- analysis/02-index-generation.R
+++ analysis/02-index-generation.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(tidyverse)
 library(sdmTMB)
 
+latitude_cutoff <- 49.93883
 bccrs <- 32609
 # loc = "HBLL INS N"
 # loc = "HBLL INS S"
@@ -29,7 +30,9 @@ coast_proj <- sf::st_transform(coast, crs = bccrs)
 # all
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds")
 sort(unique(d$year))
-grid <- readRDS("output/prediction-grid-hbll-n-s-dog.rds") # from convex hull to have deeper depths
+grid <- readRDS
+    #("output/prediction-grid-hbll-n-s-dog-1-km.rds")
+     ("output/prediction-grid-hbll-n-s-dog-2-km.rds")
 grid$area_km2 <- as.numeric(grid$area_km)
 years <- seq(min(d$year), 2023, 1)
 # grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
@@ -43,17 +46,25 @@ extratime <- c(1987, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
 plot(grid$longitude, grid$latitude)
 points(d$longitude, d$latitude, col = "red")
 
-# all without 2004 comp work
+
+# all without 2004 comp work ----------------------------------------------
+
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |>
   filter(year != 2004)
-grid <- readRDS("output/prediction-grid-hbll-n-s-dog.rds") # from convex hull to have deeper depths
+range(d$depth_m)
+d$log_botdepth2 <- d$log_botdepth * d$log_botdepth
+str(d$month)
+grid <- grid <- readRDS
+      #("output/prediction-grid-hbll-n-s-dog-1-km.rds")
+      ("output/prediction-grid-hbll-n-s-dog-2-km.rds")
+grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
 grid$area_km2 <- as.numeric(grid$area_km)
 years <- seq(min(d$year), 2023, 1)
 # grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
 grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
-grid$survey_type <- "hbll"
+grid$survey2 <- "hbll"
 grid$julian <- mean(d$julian)
-grid$month <- 7
+grid$month <- 8
 path <- "output/fit-sog-hblldog_no2004.rds"
 pathind <- "output/ind-sog-hblldog_no2004.rds"
 sort(unique(d$year))
@@ -62,11 +73,14 @@ extratime <- c(1987, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998,
 plot(grid$longitude, grid$latitude)
 points(d$longitude, d$latitude, col = "red")
 
-# hbll n only
-d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |> filter(survey_abbrev == "HBLL INS N")
+
+# hbll n only -------------------------------------------------------------
+
+d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |> filter(survey_abbrev == "HBLL INS N") |>
+  filter(survey_desc != "Dogfish Gear/Timing Comparison Surveys" )
 grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |> filter(area == "hbll_n") # from gfdata HBLL n and south merged
 min(d$latitude)
-grid <- grid |> filter(lat >=50.30182) |> filter(bot_depth <= 150) |> filter(bot_depth >= 35)
+grid <- grid |> filter(lat >=latitude_cutoff) |> filter(bot_depth <= 150) |> filter(bot_depth >= 35)
 years <- seq(min(d$year), 2023, 1)
 sort(unique(d$year))
 # grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
@@ -75,26 +89,72 @@ grid$survey_type <- "HBLL INS N"
 grid$julian <- mean(d$julian)
 path <- "output/fit-sog-hbll-n.rds"
 pathind <- "output/ind-sog-hbll-n.rds"
+sort(unique(d$year))
 extratime <- c(2005, 2006, 2009, 2011, 2013, 2015, 2017, 2018, 2020, 2022)
 
 plot(grid$lon, grid$lat)
 points(d$longitude, d$latitude, col = "red")
 
-# hblls s only
+
+# hbll s only -------------------------------------------------------------
+
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |> filter(survey_abbrev == "HBLL INS S")
-grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |> filter(area == "hbll_s") # from gfdata HBLL n and south merged
-years <- seq(min(d$year), 2023, 1)
+range(d$depth_m)
+ggplot(d, aes(depth_m, catch_count)) + geom_point()
+ggplot(d, aes(log_botdepth, catch_count)) + geom_point()
+ggplot(d, aes(longitude, latitude, colour = depth_m)) + geom_point()
+
+d$log_botdepth2 <- d$log_botdepth * d$log_botdepth
+grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |> filter(area == "hbll_s") |> filter(log_botdepth >0 ) |> filter(depth > -110)
+grid$
+grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
+# from gfdata HBLL n and south merged
+years <- seq(min(d$year), 2022, 1)
 sort(unique(d$year))
 # grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
 grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
-grid$survey_type <- "HBLL INS S"
 grid$julian <- mean(d$julian)
+ggplot(grid, aes(log_botdepth, year)) + geom_point()
+
 path <- "output/fit-sog-hbll-s.rds"
 pathind <- "output/ind-sog-hbll-s.rds"
-extratime <- c(2004,2006,2017,2020)
+extratime <- c(2006,2007, 2010, 2012, 2014, 2016, 2017, 2019, 2020)
 
 plot(grid$lon, grid$lat)
 points(d$longitude, d$latitude, col = "red")
+
+
+
+# hbll n and s only -------------------------------------------------------------
+
+d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |> filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N"))
+range(d$depth_m)
+ggplot(d, aes(depth_m, catch_count)) + geom_point()
+ggplot(d, aes(log_botdepth, catch_count)) + geom_point()
+ggplot(d, aes(longitude, latitude, colour = depth_m)) + geom_point()
+
+d$log_botdepth2 <- d$log_botdepth * d$log_botdepth
+grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |> filter(area %in% c("hbll_s", "hbll_n")) |> filter(log_botdepth >0 ) |> filter(depth > -110)
+grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
+# from gfdata HBLL n and south merged
+years <- seq(min(d$year), 2022, 1)
+sort(unique(d$year))
+# grid <- purrr::map_dfr(unique(d$year), ~ tibble(grid, year = .x))
+grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
+grid$julian <- mean(d$julian)
+ggplot(grid, aes(log_botdepth, year)) + geom_point()
+
+path <- "output/fit-sog-hbll-s.rds"
+pathind <- "output/ind-sog-hbll-s.rds"
+extratime <- c(2006,2007, 2010, 2012, 2014, 2016, 2017, 2019, 2020)
+
+plot(grid$lon, grid$lat)
+points(d$longitude, d$latitude, col = "red")
+
+
+
+# dog survey only ---------------------------------------------------------
+
 
 # dog survey only - need a prediction grid for just DOG points
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |> filter(survey_abbrev %in% c("HBLL INS S", "dog", "dog-jhook"))
@@ -115,7 +175,8 @@ plot(grid$longitude, grid$latitude)
 points(d$longitude, d$latitude, col = "red")
 
 
-# dog survey only - need a prediction grid for just DOG points
+# dog survey only ---------------------------------------------------------
+# need a prediction grid for just DOG points
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |> filter(survey_abbrev %in% c("dog"))
 max(d$latitude)
 range(d$year)
@@ -137,28 +198,36 @@ points(d$longitude, d$latitude, col = "red")
 
 
 
-# rm 2004 calibration work??
-# rm <- filter(d, year == 2004 & survey_abbrev %in% c("dog-jhook", "dog")) #the catch rates are so #low and I don't know what the soak time was
-# d <- filter(d, !(fishing_event_id %in% rm$fishing_event_id))
+# plots to check data -----------------------------------------------------
+
 
 ggplot(d, aes(longitude, latitude, colour = survey_abbrev)) +
   geom_point() + theme_classic()
 
 d |>
   ggplot( ) +
-  geom_jitter(aes(year, catch_count, colour = survey_type, size = catch_count))
+  geom_jitter(aes(year, catch_count, colour = survey3, size = catch_count))
 
 d |>
   ggplot( ) +
-  geom_point(aes(log_botdepth, catch_count,   colour = survey_type))
+  geom_point(aes(longitude, latitude, colour = catch_count/offset)) + facet_wrap(~year) +
+  scale_colour_viridis_c()
 
 d |>
   ggplot( ) +
-  geom_point(aes(catch_count, offset,  colour = survey_type))
+  geom_point(aes(log_botdepth, catch_count,   colour = survey2))
 
 d |>
   ggplot( ) +
-  geom_point(aes(catch_count, log_botdepth,  colour = survey_type))
+  geom_point(aes(log_botdepth, catch_count,   colour = survey_abbrev))
+
+d |>
+  ggplot( ) +
+  geom_point(aes(catch_count, offset,  colour = survey3))
+
+d |>
+  ggplot( ) +
+  geom_point(aes(catch_count, log_botdepth,  colour = survey3))
 
 d |>
   group_by(year) |>
@@ -167,16 +236,26 @@ d |>
   geom_point(aes(year, catch)) +
   geom_line(aes(year, catch))
 
+d |>
+  group_by(year, survey3) |>
+  summarise(cpue = sum(catch_count/offset)) |>
+  ggplot( ) +
+  geom_point(aes(year, cpue, group = survey3)) +
+  geom_line(aes(year, cpue, group = survey3))
+
 range(grid$bot_depth)
 range(grid$log_botdepth)
+range(grid$bot_depth)
 range(d$depth_m)
 range(d$log_botdepth)
+range(d$catch_count)
+d <- na.omit(d)
 
-# make this a function so that I can create index for HBLL N, HBLL --------
+# index generation function --------
 
 indexfunc <- function(d) {
   mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 5)
-  #mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 2) #hbll n only
+  #mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 2) #hbll s only
   plot(mesh)
   mesh$mesh$n
 
@@ -214,12 +293,12 @@ indexfunc <- function(d) {
   # # ggsave("Figures/mesh.pdf", width = 6, height = 6)
 
   unique(sort(d$year))
-  unique(sort(d$survey_type))
+  unique(sort(d$survey2))
 
   fit <- sdmTMB(
     formula =
-    catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type),
-    #catch_count ~ poly(log_botdepth, 2) , #hbll n model, hbll s model
+    catch_count ~ poly(log_botdepth, 2) + as.factor(survey2),
+    #catch_count ~ log_botdepth + log_botdepth2, #hbll n model, hbll s model
     data = d,
     time = "year",
     offset = "offset",
@@ -237,19 +316,18 @@ indexfunc <- function(d) {
 
   fit$sd_report
   sanity(fit)
-  # fitjul <- update(fit, formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type) + poly(julian, 2))
-   fitmonth <- update(fit, formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_type) + poly(month, 2))
-   sanity(fitmonth)
-   fitmonth$sd_report
-   # saveRDS(fitmonth, file = "output/fit-sog-hblldog-month.rds")
-   fitmonth <- readRDS(file = "output/fit-sog-hblldog-month.rds")
 
-   pred <- predict(fitmonth, grid, return_tmb_object = TRUE, response = TRUE)
-   index <- get_index(pred, bias_correct = TRUE)
-   saveRDS(index, file = "output/ind-sog-hblldog-month.rds")
+  # fitmonth <- update(fit, formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey2) + poly(month, 2))
+  # sanity(fitmonth)
+  # fitmonth$sd_report
+  # saveRDS(fitmonth, file = "output/fit-sog-hblldog-month.rds")
+  # fitmonth <- readRDS(file = "output/fit-sog-hblldog-month.rds")
+  # pred <- predict(fitmonth, grid, return_tmb_object = TRUE, response = TRUE)
+  # index <- get_index(pred, bias_correct = TRUE)
+  # index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
+  # saveRDS(index, file = "output/ind-sog-hblldog-month.rds")
 
-   saveRDS(fit, file = path)
-  # saveRDS(fitjul, file = "output/fit-sog-hblldog-julian.rds")
+  saveRDS(fit, file = path)
 
   pred <- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
   index <- get_index(pred, bias_correct = TRUE)
@@ -263,10 +341,10 @@ indexfunc <- function(d) {
   yearlabs <- yearlabs$year
     #c(1986, 1989, 2005, 2008, 2009, 2011, 2013, 2014, 2015, 2018, 2019, 2021, 2022, 2023)
 
-  ggplot(index, aes(year, est / 10000)) +
+  ggplot(index, aes(year, est)) +
     geom_line(col = "#8D9999") +
     geom_point(col = "#8D9999") +
-    geom_ribbon(aes(ymin = lwr / 10000, ymax = upr / 10000), alpha = 0.4, fill = "#8D9999") +
+    geom_ribbon(aes(ymin = lwr , ymax = upr ), alpha = 0.4, fill = "#8D9999") +
     theme_classic() +
     scale_x_continuous(breaks = c(years))
 
@@ -279,3 +357,9 @@ indexfunc <- function(d) {
 
 
 }
+
+
+# run functions -----------------------------------------------------------
+
+
+indexfunc(d)
