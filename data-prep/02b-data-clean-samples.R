89dad664abe488e61d5d9f93091b125f78199cbf
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-07-19 10:48:40 -0700

    change survey names to have three options

--- data-prep/02b-data-clean-samples.R
+++ data-prep/02b-data-clean-samples.R
@@ -39,25 +39,43 @@ unique(samples$survey_desc)
 unique(x$year)
 
 samples <- samples |>
-  mutate(survey = case_when(
+  mutate(survey3 = case_when(
     year %in% c(1986, 1989) ~ "dog-jhook",
     year %in% c(2005, 2008, 2011, 2014) ~ "dog",
     year == 2019 & survey_desc == "2019 Strait of Georgia Longline Dogfish Survey" ~ "dog",
-    year == 2019 & survey_desc == "2019 Dogfish Gear/Timing Comparison Survey" & hooksize_desc == "13/0" ~ "HBLL INS S",
-    year == 2019 & survey_desc == "2019 Dogfish Gear/Timing Comparison Survey" & hooksize_desc == "14/0" ~ "dog",
+    year == 2019 & survey_desc == "2019 Dogfish Gear/Timing Comparison Survey" & hooksize_desc == "13/0" ~ "HBLL INS S comp",
+    year == 2019 & survey_desc == "2019 Dogfish Gear/Timing Comparison Survey" & hooksize_desc == "14/0" ~ "dog comp",
     year == 2023 & survey_desc == "2023 Dogfish Gear Comparison Survey" & hooksize_desc == "14/0" ~ "dog",
-    year == 2023 & survey_desc == "2023 Dogfish Gear Comparison Survey" & hooksize_desc == "13/0" ~ "HBLL INS S",
-    year == 2023 & survey_desc == "The 2023 Summer Dogfish gear comparison survey on the Neocaligus." & hooksize_desc == "14/0" ~ "dog",
-    year == 2023 & survey_desc == "The 2023 Summer Dogfish gear comparison survey on the Neocaligus." & hooksize_desc == "13/0" ~ "HBLL INS S",
+    year == 2023 & survey_desc == "2023 Dogfish Gear Comparison Survey" & hooksize_desc == "13/0" ~ "HBLL INS S comp",
+    year == 2023 & survey_desc == "The 2023 Summer Dogfish gear comparison survey on the Neocaligus." & hooksize_desc == "14/0" ~ "dog comp",
+    year == 2023 & survey_desc == "The 2023 Summer Dogfish gear comparison survey on the Neocaligus." & hooksize_desc == "13/0" ~ "HBLL INS S comp",
     year == 2023 & survey_desc == "2023 Dogfish Gear Comparison Survey" & hooksize_desc == "12/0" ~ "dog-jhook",
     year == 2022 & hooksize_desc == "13/0" ~ "HBLL INS S",
-    year == 2022 & hooksize_desc == "14/0" ~ "dog",
+    year == 2022 & hooksize_desc == "14/0" ~ "dog comp",
     year == 2004 & hooksize_desc == "14/0" ~ "dog",
     year == 2004 & hooksize_desc == "12/0" ~ "dog-jhook"
   ))
 
 
-x <- filter(samples, is.na(survey) == TRUE)
+samples <- samples |>
+  mutate(survey2 = case_when(
+    survey3 == "dog-jhook" ~ "dog-jhook",
+    survey3 == "dog" ~ "dog",
+    survey3 == "HBLL INS S comp" ~ "hbll",
+    survey3 == "dog comp" ~ "dog",
+    survey3 == "HBLL INS S" ~ "hbll"
+    ))
+
+samples <- samples |>
+  mutate(survey_abbrev = case_when(
+    survey3 == "dog-jhook" ~ "dog-jhook",
+    survey3 == "dog" ~ "dog",
+    survey3 == "HBLL INS S comp" ~ "HBLL INS S",
+    survey3 == "dog comp" ~ "dog",
+    survey3 == "HBLL INS S" ~ "HBLL INS S"
+  ))
+
+x <- filter(samples, is.na(survey3) == TRUE)
 unique(x$survey_desc)
 unique(x$fishing_event_id) # why does this one fishing event not have a parent event id??, this is also not found in the sets dataframe
 
