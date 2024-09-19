a98d2410723e9b485bfe9102bd9cd7e9790f503f
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-09-19 15:25:04 -0700

    align get_all and gfdata data pull for HBLL sets

--- data-prep/03-wrangle-hbll-dog-sets.R
+++ data-prep/03-wrangle-hbll-dog-sets.R
@@ -3,7 +3,7 @@
 # 02a-data-clean-sets.R
 # soak2005 <- 2
 bccrs <- 32609
-latitude_cutoff <- 49.99607
+latitude_cutoff <- 50.32000 #<- not sure what the "best" boundary to pick for separating n and s is
 
 library(sf)
 library(ggplot2)
@@ -13,33 +13,13 @@ library(sdmTMB)
 # load data  ---------------------------------------------------------------
 
 final <- readRDS("data-generated/dogfish_sets_cleaned_getall.rds")
-hbll <- filter(final, survey_lumped == "hbll") |> filter(usability_code == 1 ) |> filter(survey_series_og %in% c(39, 40)) # note how the boundary has been different, also this is from the *get_all* function that pulls in survey locations that are not a part of the standardized survey, remove them
-hbllsets <- get_survey_sets(species = "north pacific spiny dogfish", ssid = c(39,40))
+final <-
+hbll <- filter(final, survey_lumped == "hbll") |>
+  filter(usability_code == 1 ) |>
+  filter(survey_series_og %in% c(39, 40)) # note how the boundary has been different, also this is from the *get_all* function that pulls in survey locations that are not a part of the standardized survey, remove them
+hbllgfdata <- get_survey_sets(species = "north pacific spiny dogfish", ssid = c(39,40))
 
-ggplot(hbll) +
-  geom_point(aes(longitude, latitude)) +
-  facet_wrap(~survey_abbrev)
-
-testpe <- hbll |>
-  filter(survey_abbrev == "HBLL INS N") |>
-  filter(year == 2007) |>
-  dplyr::select(year, fishing_event_id, survey_abbrev, catch_count)
-
-# ggplot() +
-#   geom_point(aes(longitude, latitude, colour = catch_count)) +
-#   facet_wrap(~year)
-
-test <- hbllsets |>
-  filter(survey_abbrev == "HBLL INS N") |>
-  filter(year == 2007) |>
-  dplyr::select(year, fishing_event_id, survey_abbrev, catch_count) |>
-  mutate(id = "gf")
-
-ggplot() +
-  geom_point(aes(longitude, latitude, colour = catch_count)) +
-  facet_wrap(~year)
-
-testpe2 <- left_join(testpe, test)
+hbll <- filter(hbll, fishing_event_id %in% c(hbllgfdata$fishing_event_id))
 
 hbllgf <- hbllsets |> group_by(year) |> filter(survey_series_id %in% c(39)) |> drop_na(catch_count) |> reframe(sum = sum(catch_count))
 hbllpe <- hbll |> filter(survey_series_id %in% c(39)) |> group_by(year) |> drop_na(catch_count) |> reframe(sumpe = sum(catch_count))
@@ -49,18 +29,10 @@ x$diff <- x$sum - x$sumpe #slight discripancies between the two different data p
 
 ggplot(x) +
   geom_line(aes(year, sum), col = "red") +
-  geom_line(aes(year, sumpe)) #slight discripancies between the two different data pulls. Could the be the points that extend around may be partially. 2007 is in the in N
+  geom_line(aes(year, sumpe)) #slight discrepancies between the two different data pulls. Could the be the points that extend around may be partially. 2007 is in the in N and there are two extra points in the get_all data pull. THey are removed now to match the gfdata pull.
 
 
 # hbll wrangle ------------------------------------------------------------
-
-ggplot(final) +
-  geom_point(aes(longitude, latitude)) +
-  facet_wrap(~survey_sep)
-
-ggplot(final) +
-  geom_point(aes(longitude, latitude)) +
-  facet_wrap(~survey_lumped)
 
 final |>
   group_by(survey_lumped, year) |>
@@ -91,14 +63,16 @@ hbll <- hbll |>
 
 ggplot(hbll, aes(longitude, latitude, colour = survey_abbrev)) +
   geom_point() +
-  facet_wrap(~survey_abbrev)
+  facet_wrap(~survey_abbrev + year)
 
 ggplot(hbll, aes(longitude, latitude, colour = survey_sep)) +
   geom_point() +
   facet_wrap(~survey_sep)
+
 ggplot(hbll, aes(longitude, latitude, colour = survey_sep)) +
   geom_point() +
   facet_wrap(~year)
+
 ggplot(hbll, aes(longitude, latitude, colour = survey_lumped)) +
   geom_point() +
   facet_wrap(~survey_lumped)
@@ -117,6 +91,7 @@ final |>
 ggplot(final, aes(longitude, latitude, colour = survey_lumped)) +
   geom_point() +
   facet_wrap(~survey_lumped)
+
 ggplot(final, aes(longitude, latitude, colour = survey_sep)) +
   geom_point() +
   facet_wrap(~survey_sep)
