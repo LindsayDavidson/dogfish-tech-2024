c9a4d37d1b2f9d6947fe93c033da6ab6d1e56b55
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-09-19 15:25:25 -0700

    change name in plots

--- data-prep/04-summary-set-plots.R
+++ data-prep/04-summary-set-plots.R
@@ -7,8 +7,9 @@ cols <- c("#d7191c", "#fdae61", "#2c6184",  "#2c7bb6" )
 # load data ---------------------------------------------------------------
 
 d <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") #no west coast VI expansion set
-#d <- readRDS("data-raw/wrangled-hbll-dog-sets-hblls.rds") #no expansion set, no hbll north execpt for the 2008 year, note 2004 got dropped when we dropped NAs in soak time
-
+#d <- readRDS("data-raw/wrangled-hbll-dog-sets-hblls.rds") #no expansion set, no hbll north except for the 2008 year, note 2004 got dropped when we dropped NAs in soak time
+d <- filter(d, soak >= 0)
+d <- filter(d, is.na(soak) != TRUE) #get rid of 2004 that has no soak time
 
 # load map ----------------------------------------------------------------
 library(sf)
@@ -22,21 +23,36 @@ bc_coast <- st_crop(
 # summary plots -----------------------------------------------------------
 
 ggplot() +
-  geom_point(data = d, aes(longitude, latitude, colour = survey2)) +
+  geom_point(data = d, aes(longitude, latitude, colour = survey_lumped)) +
   theme_classic() +
-  geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +    facet_wrap(~survey2) +
+  geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +    facet_wrap(~survey_lumped) +
   guides(colour=guide_legend(title="Survey"))
 
 ggplot() +
-  geom_point(data = d, aes(longitude, latitude, colour = survey_abbrev)) +
+  geom_point(data = d, aes(longitude, latitude, colour = survey_sep)) +
   theme_classic() +
-  geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +    facet_wrap(~survey_abbrev) +
+  geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +    facet_wrap(~survey_lumped) +
+  guides(colour=guide_legend(title="Survey"))
+
+d |>
+  group_by(survey_lumped, year) |>
+  distinct() |>
+  reframe() |>
+  print(n=40) #looks good
+
+d |>
+  filter(survey_sep != "dog comp") |>
+  ggplot() +
+  geom_point(aes(longitude, latitude, colour = survey_sep)) +
+  theme_classic() +
+  geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +
+  facet_wrap(~survey_sep) +
   guides(colour=guide_legend(title="Survey"))
 ggsave("Figures/summary_locations.png", width = 5, height = 4)
 
 
 d |>
-  filter(survey_abbrev == "HBLL INS S") |>
+  filter(survey_sep == "HBLL INS S") |>
   ggplot() +
   geom_point(aes(longitude, latitude, colour = catch_count), size = 1) +
   theme_classic() +
@@ -48,7 +64,7 @@ ggsave("Figures/summary_locations_hbllinss.png", width = 5, height = 4)
 
 
 d |>
-  filter(survey_abbrev == "HBLL INS N") |>
+  filter(survey_sep == "HBLL INS N") |>
   ggplot() +
   geom_point(aes(longitude, latitude, colour = catch_count), size = 1) +
   theme_classic() +
@@ -60,7 +76,7 @@ ggsave("Figures/summary_locations_hbllinsn.png", width = 5, height = 4)
 
 
 d |>
-  filter(survey_abbrev == "dog") |>
+  filter(survey_sep %in% c("dog", "dog-jhook")) |>
   ggplot() +
   geom_point(aes(longitude, latitude, colour = catch_count), size = 1) +
   theme_classic() +
@@ -71,7 +87,7 @@ d |>
 ggsave("Figures/summary_locations_dog.png", width = 5, height = 4)
 
 d |>
-  filter(survey_abbrev == "dog-jhook") |>
+  filter(survey_lumped == "dog-jhook") |>
   ggplot() +
   geom_point(aes(longitude, latitude, colour = catch_count), size = 1) +
   theme_classic() +
@@ -82,41 +98,47 @@ d |>
 ggsave("Figures/summary_locations_dogjhook.png", width = 5, height = 4)
 
 
-
 d |>
-  filter(survey2 != "hbll comp") |>
-  filter(survey2 != "dog comp") |>
+  filter(survey_sep != "hbll comp") |>
+  filter(survey_sep != "dog comp") |>
   group_by(survey_abbrev, year) |>
+  drop_na(offset) |>
+  drop_na(catch_count) |>
   mutate(
     catch_count_sum = sum(catch_count),
     cpue = sum(catch_count) / sum(exp(offset)),
-    cpue_hk = sum(catch_count) / sum(hook_count)
+    cpue_hk = sum(catch_count) / sum(lglsp_hook_count)
   ) |>
   ggplot() +
-  geom_point(aes(year, cpue, colour = survey_abbrev, size = 1)) +
-  geom_line(aes(year, cpue, colour = survey_abbrev), size = 1) + theme_classic() +
+  geom_point(aes(year, cpue, colour = survey_sep, size = 1)) +
+  geom_line(aes(year, cpue, colour = survey_sep), size = 1) + theme_classic() +
+  #scale_color_manual(values = cols) +
+  guides(colour=guide_legend(title="Survey")) +
+  scale_colour_viridis_d()
+#ggsave("Figures/summary_cpuetrends.png", width = 5, height = 4)
+
+d |>
+  filter(survey_sep != "hbll comp") |>
+  filter(survey_sep != "dog comp") |>
+  group_by(survey_sep, year) |>
+  drop_na(offset) |>
+  drop_na(catch_count) |>
+  mutate(
+    catch_count_sum = sum(catch_count),
+    cpue = sum(catch_count) / sum(exp(offset)),
+    cpue_hk = sum(catch_count) / sum(lglsp_hook_count)
+  ) |>
+  ggplot() +
+  geom_point(aes(year, cpue, colour = survey_sep, size = 1)) +
+  geom_line(aes(year, cpue, colour = survey_sep), size = 1) + theme_classic() +
   #scale_color_manual(values = cols) +
   guides(colour=guide_legend(title="Survey")) +
   scale_colour_viridis_d()
 ggsave("Figures/summary_cpuetrends.png", width = 5, height = 4)
 
 d |>
-  group_by(survey2, year) |>
-  mutate(
-    catch_count_sum = sum(catch_count),
-    cpue = sum(catch_count) / sum(exp(offset)),
-    cpue_hk = sum(catch_count) / sum(hook_count)
-  ) |>
-  ggplot() +
-  geom_point(aes(year, cpue, colour = survey3, size = 1)) +
-  geom_line(aes(year, cpue, colour = survey3), size = 1) + theme_classic() +
-  #scale_color_manual(values = cols) +
-  guides(colour=guide_legend(title="Survey")) +
-  scale_colour_viridis_d()
-
-d |>
-  filter(survey2 != "hbll comp") |>
-  filter(survey2 != "dog comp") |>
+  filter(survey_sep != "hbll comp") |>
+  filter(survey_sep != "dog comp") |>
   #filter(survey_abbrev == "hbll") |>
   group_by(survey_abbrev, year) |>
   ggplot() +
@@ -126,8 +148,8 @@ d |>
 
 d |>
   ggplot() +
-  geom_point(aes(longitude, latitude, colour = survey2)) +
-  facet_wrap(~year + survey2) +
+  geom_point(aes(longitude, latitude, colour = survey_sep)) +
+  facet_wrap(~year + survey_sep) +
   theme_classic() +
   #scale_color_manual(values = cols) +
   guides(colour=guide_legend(title="Survey")) +
@@ -137,7 +159,7 @@ ggsave("Figures/summary_surveylocations.png", width = 10, height = 10)
 d |>
   ggplot() +
   geom_point(aes(longitude, latitude, colour = catch_count, fill = catch_count), alpha = 0.5, size = 0.5) +
-  facet_wrap(~year + survey2) +
+  facet_wrap(~year + survey_lumped) +
   scale_colour_viridis_c(trans = "sqrt") +
   scale_fill_viridis_c(trans = "sqrt") +
   theme_classic() +
@@ -147,7 +169,7 @@ ggsave("Figures/summary_surveylocationscatches.png", width = 10, height = 10)
 d |>
   ggplot() +
   geom_point(aes(longitude, latitude, colour = catch_count, fill = catch_count, size = catch_count), alpha = 0.5) +
-  facet_wrap(~year + survey2) +
+  facet_wrap(~year + survey_lumped) +
   scale_colour_viridis_c(trans = "sqrt") +
   scale_fill_viridis_c(trans = "sqrt") +
   theme_classic()
@@ -163,17 +185,6 @@ d |>
   geom_sf(data = bc_coast, fill = "grey90", colour = "grey70")
 ggsave("Figures/summary_surveylocationscatches_yronly.png", width = 8, height = 6)
 
-test <- filter(d, year == 2023 & survey_abbrev %in% c("hbll", "HBLL INS S"))
-test <- test |> mutate(time = ifelse(julian > 265, "late", "early"))
-test2 <- filter(d, year == 2021 & survey_abbrev == "HBLL INS S")
-test2$time <- "early"
-test3 <- bind_rows(test, test2)
-test3 |>
-  ggplot() +
-  geom_jitter(aes(year, (catch_count), colour = time), alpha = 0.5) +
-  theme_classic()
-
-
 d |>
   group_by(survey_abbrev, year) |>
   ggplot() +
@@ -187,16 +198,15 @@ ggsave("Figures/summary_catches.png", width = 6, height = 5)
 d |>
   group_by(survey_abbrev, year) |>
   ggplot() +
-  geom_jitter(aes(year, hook_count, colour = survey_abbrev), alpha = 0.5) +
+  geom_jitter(aes(year, lglsp_hook_count, colour = survey_abbrev), alpha = 0.5) +
   theme_classic()
-  #scale_color_manual(values = cols)
 ggsave("Figures/summary_hookscounts.png", width = 5, height = 4)
 
 
 d |>
-  group_by(survey2,survey_abbrev, year) |>
+  group_by(survey_sep,survey_abbrev, year) |>
   ggplot() +
-  geom_jitter(aes(year, julian, colour = survey2), alpha = 0.5) +
+  geom_jitter(aes(year, julian, colour = survey_sep), alpha = 0.5) +
   theme_classic() +
   guides(size="none") +
   #scale_color_manual(values = cols) +
@@ -236,12 +246,4 @@ d |>
   #scale_color_manual(values = cols)
 ggsave("Figures/summary_depth.png", width = 6, height = 5)
 
-d |>
-  filter(survey2 == "hbll") |>
-  group_by(survey_abbrev, year) |>
-  ggplot() +
-  geom_jitter(aes(log_botdepth,log(catch_count), colour = survey_abbrev), alpha = 0.5) +
-  facet_wrap(~year) +
-  theme_classic()
-  #scale_color_manual(values = cols)
 
