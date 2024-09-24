bfb39780eb00105228ebb2cb5feb46bfea7a1245
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-09-24 10:25:27 -0700

    erase summary plots

--- data-prep/01b-pull-data-get_all.R
+++ data-prep/01b-pull-data-get_all.R
@@ -24,66 +24,14 @@ library(sp)
 library(gfdata)
 remotes::install_github("pbs-assess/gfdata", ref = "trials", force = TRUE)
 
-# pull inside samples data ------------------------------------------------
+# pull inside sets data ------------------------------------------------
 d <- get_all_survey_sets("north pacific spiny dogfish", ssid = c(76, 48, 39, 40))
 unique(d$survey_abbrev)
 saveRDS(d, "data-raw/dogfish_sets_getall.rds")
 dim(filter(d, fishing_event_id == 5883186)) #duplicate?? should be 2 not 4
 
-d <- readRDS("data-raw/dogfish_sets_getall.rds")
-
-# final |>
-#   drop_na(catch_count, lglsp_hook_count) |>
-#   group_by(year, survey2) |>
-#   reframe(catchsum = sum(catch_count), hooksum = sum(lglsp_hook_count)) |>
-#   mutate(cpue = catchsum/hooksum) |>
-#   ggplot() +
-#   geom_point(aes(year, cpue, colour = survey2, group = survey2)) +
-#   geom_line(aes(year, cpue, colour = survey2, group = survey2))
-#
-# final |>
-#   drop_na(catch_count, lglsp_hook_count) |>
-#   group_by(year, survey) |>
-#   reframe(catchsum = sum(catch_count), hooksum = sum(lglsp_hook_count)) |>
-#   mutate(cpue = catchsum/hooksum) |>
-#   ggplot() +
-#   geom_point(aes(year, cpue, colour = survey, group = survey)) +
-#   geom_line(aes(year, cpue, colour = survey, group = survey))
-
-# ggplot() +
-#   geom_point(data = final, aes(longitude, latitude, colour = survey2)) +
-#   theme_classic() +
-#   geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +
-#   facet_wrap(~survey2) +
-#   guides(colour=guide_legend(title="Survey"))
-#
-# ggplot() +
-#   geom_point(data = final, aes(longitude, latitude, colour = survey_abbrev)) +
-#   theme_classic() +
-#   geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +
-#   facet_wrap(~survey_abbrev) +
-#   guides(colour=guide_legend(title="Survey"))
-#
-#
-# final |>
-#   filter(survey_abbrev == "DOG") |>
-#   ggplot() +
-#   geom_point(aes(longitude, latitude, colour = catch_count), size = 1) +
-#   theme_classic() +
-#   geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +
-#   facet_wrap(~year) +
-#   guides(colour=guide_legend(title="Catch count")) +
-#   scale_colour_viridis_c() +
-#   theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))
-#
-# final |>
-#   filter(survey2 == "dog-jhook") |>
-#   ggplot() +
-#   geom_point(aes(longitude, latitude, colour = catch_count), size = 1) +
-#   theme_classic() +
-#   geom_sf(data = bc_coast, fill = "grey90", colour = "grey70") +
-#   facet_wrap(~year) +
-#   guides(colour=guide_legend(title="Catch count")) +
-#   scale_colour_viridis_c() +
-#   theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))
-#
+#pull inside samples data
+#d <- get_all_survey_samples("north pacific spiny dogfish", ssid = c(76, 48, 39, 40))
+d <- get_all_survey_samples("north pacific spiny dogfish", ssid = c(76, 48, 39, 40), include_event_info = TRUE)
+unique(d$survey_abbrev)
+saveRDS(d, "data-raw/dogfish_samples_getall.rds")
