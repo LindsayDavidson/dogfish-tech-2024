1d51529b01d1e24f538ee9e3d9191edeadf9541f
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-09-24 11:15:54 -0700

    rm code for sql pull

--- data-prep/03a-wrangle-hbll-dog-samples.R
+++ data-prep/03a-wrangle-hbll-dog-samples.R
@@ -16,28 +16,37 @@ library(sf)
 library(sp)
 
 # load data ---------------------------------------------------------------
-# samps <- readRDS("output/dogfish_samps.rds")
-hsamps <- readRDS("data-raw/samples-hbll-dog.rds") #just hbll samples
+samps <- readRDS("output/dogfish_samps.rds")
+hsamps <- readRDS("data-raw/samples-hbll-dog.rds") # just hbll samples
 names(hsamps)
 
 samps <- readRDS("data-raw/dogfish_samples_cleaned.rds")
 sampshb <- samps |> filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N"))
+# drop the hbll samples not included in the gfdata pull.
+sampsgf <- readRDS("data-raw/dogfish_samples_gfdata.rds") |> filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N"))
+
 sampshb <- sampshb |>
+  filter(fishing_event_id %in% c(sampsgf$fishing_event_id)) |>
   drop_na(total_length) |>
-  filter(total_length > 10) #i suspect these smaller ones are pups that were measured
-ggplot(sampshb, aes(longitude, latitude, colour = total_length)) + geom_point()
+  filter(total_length > 10) # i suspect these smaller ones are pups that were measured
+ggplot(sampshb, aes(longitude, latitude, colour = total_length)) +
+  geom_point()
 
-# #dsamps <- readRDS("data-raw/dogfish_samples_cleaned.rds") |>
-# dsamps <- readRDS("data-raw/dogfish_samples_cleaned_withmaturity.rds") |>
-#   filter(species_common_name == "NORTH PACIFIC SPINY DOGFISH") |>
-#   drop_na(total_length) |>
-#   filter(total_length > 0) |>
-#   mutate(total_length = total_length/10)
-
-# # set data (to remove fishing sets)
-# hbll <- readRDS("data-raw/hbllsets.rds")
-# final <- readRDS("data-generated/dogfish_sets_cleaned.rds") |>
-#   filter(species_code == "044") # pull just dogfish data not other species
+# check the databases are returning the same <- looks good
+# sampsgf <- readRDS("data-raw/dogfish_samples_gfdata.rds") |>
+#   filter(!survey_abbrev %in% c("HBLL INS S", "HBLL INS N")) |>
+#   filter(length >0 )
+# sampsga <- samps |>
+#   filter(!survey_abbrev %in% c("HBLL INS S", "HBLL INS N")) |>
+#   filter(length >0 ) |>
+#   filter(fishing_event_id %in% c(sampsgf$fishing_event_id))
+# length(unique(sampsga$fishing_event_id))
+# length(unique(sampsgf$fishing_event_id))
+# length(unique(sampsga$year))
+# length(unique(sampsgf$year))
+# x <- ggplot(sampsgf, aes(year, length)) + geom_point()
+# ggplot(sampsga, aes(year, length)) + geom_point()
+# x + geom_point(data = sampsga, aes(year, total_length), colour = "red")
 
 # remove two survey years that extended along the west coast VI
 hbll1 <- filter(sampshb, (latitude < 48.5 & longitude < -123.3)) # only two years have the sampling around the strait
@@ -55,72 +64,23 @@ x + geom_point(data = hbllrm, aes(longitude, latitude), col = "red")
 
 hsamps <- sampshb |>
   filter(!fishing_event_id %in% c(hbllrm$fishing_event_id))
-# names(hsamps)
-#
-# hsamps2 <- hsamps |>
-#   dplyr::select(
-#   survey_abbrev, year, month, species_common_name,
-#   sex, length, weight, fishing_event_id, survey_abbrev,
-#   trip_start_date, usability_code,
-#   sample_id, maturity_code, maturity_name, maturity_desc,
-#   maturity_convention_code, maturity_convention_desc, maturity_convention_maxvalue,
-#   age
-# ) |> mutate(
-#   hook_desc = "CIRCLE HOOK", hooksize_desc = "13/0", activity_desc = "HBLL",
-# )
-
-# hsamps2 <- hsamps2 |>
-#   mutate(survey2 = case_when(
-#     survey_abbrev == "HBLL INS S" ~ "hbll",
-#     survey_abbrev == "HBLL INS N" ~ "hbll"
-#   ))
-#
-# hsamps2 <- hsamps2 |>
-#   mutate(survey3 = case_when(
-#     survey_abbrev == "HBLL INS S" ~ "HBLL INS S",
-#     survey_abbrev == "HBLL INS N" ~ "HBLL INS N"
-#   ))
-
-# dsamps <-
-#   dsamps |>
-#   mutate(
-#     date= as.Date(trip_start_date, format = "%Y-%m-%d"),
-#     julian = lubridate::yday(date),
-#     month = lubridate::month(date))
-
-
-# dsamps2 <- dsamps |>
-#   dplyr::select(
-#     year, month, species_common_name, usability_code, trip_start_date,
-#     specimen_sex_code, total_length, round_weight, fishing_event_id, month, fishing_event_id, hooksize_desc, hook_desc, activity_desc, survey2, survey3, survey_abbrev, sample_id, specimen_id,
-#     maturity_code, maturity_name, maturity_desc,
-#     maturity_convention_code, maturity_convention_desc, maturity_convention_maxvalue,
-#     specimen_age
-#   ) |>
-#   rename(
-#     sex = specimen_sex_code,
-#     age = specimen_age,
-#     length = total_length,
-#     weight = round_weight
-#   )
-
-# samps <- bind_rows(dsamps2, hsamps2)
 
 saveRDS(samps, "output/samps_joined.rds")
 
 
 # summary plot ------------------------------------------------------------
 
-samps <- readRDS("output/samps_joined.rds")
-
 ggplot(samps, aes(year, length, col = survey_abbrev)) +
-  geom_jitter() + facet_wrap(~survey_abbrev)
+  geom_jitter() +
+  facet_wrap(~survey_abbrev)
 
 ggplot(samps, aes(year, maturity_code, col = survey_abbrev)) +
-  geom_jitter() + facet_wrap(~survey_abbrev)
+  geom_jitter() +
+  facet_wrap(~survey_abbrev)
 
 ggplot(samps, aes(year, maturity_code, col = survey_abbrev)) +
-  geom_jitter() + facet_wrap(~survey_abbrev)
+  geom_jitter() +
+  facet_wrap(~survey_abbrev)
 
 ggplot(samps, aes(year, length, col = survey_abbrev)) +
   geom_jitter() +
@@ -129,4 +89,3 @@ ggplot(samps, aes(year, length, col = survey_abbrev)) +
 ggplot(samps, aes(year, length, col = hook_desc)) +
   geom_jitter() +
   facet_wrap(~sex)
-
