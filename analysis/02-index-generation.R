8af9a7bfd07d7613b7a099f38af2bd19893cd3f7
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-10-03 17:01:27 -0700

    add params so all models converge

--- analysis/02-index-generation.R
+++ analysis/02-index-generation.R
@@ -44,7 +44,7 @@ df <- df |>
   ))
 
 grid <- readRDS("output/prediction-grid-hbll-n-s-dog-2-km.rds") # ("output/prediction-grid-hbll-n-s-dog-1-km.rds")
-
+grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
 grid$area_km2 <- as.numeric(grid$area_km)
 grid$depth_m <- grid$depth * -1
 grid$julian_c <- 36
@@ -65,11 +65,11 @@ grid <- grid |>
 # run index ---------------------------------------------------------------
 
 # params
-model <- "hblldog_no2004"
+# model <- "hblldog_no2004"
 # model = "hbll-n-s"
 # model = "hbll-n"
 # model = "hbll-s"
-# model = "hbll-dog"
+model <- "dog" #<- cannot get this model to converge
 
 if (model == "hblldog_no2004") { #<- everything except for dogfish comp work in 2004
   d <- df #<- 2004 was removed above with the offset can't be na.
@@ -99,18 +99,18 @@ if (model == "hbll-n-s") {
 
   grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |>
     filter(area %in% c("hbll_s", "hbll_n")) # from gfdata HBLL n and south merged
-  ggplot(grid, aes(lon, lat, colour = area)) +
-    geom_point()
+  # grid <- readRDS(paste0("output/prediction-grid-hbll-n-s-dog-", "0.5", "-km.rds"))
+  # range(grid$bot_depth)  #<- does this grid (smaller grid size) reduce the error bars??
   years <- seq(min(d$year), 2023, 1)
   grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
-  grid$survey_type <- "hbll"
+  grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
   formula <- catch_count ~ 1
   formuladepth <- catch_count ~ log_botdepth + log_botdepth2
-  }
+}
 
 if (model == "hbll-s") {
   d <- df |>
-    filter(survey_sep %in% c("HBLL INS S")) |> # should I removed survey_abbrev = other?
+    filter(survey_sep %in% c("HBLL INS S")) |> # should survey_abbrev != other?
     drop_na(offset) |>
     drop_na(depth_m) |>
     drop_na(catch_count)
@@ -126,17 +126,18 @@ if (model == "hbll-s") {
 
   grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |>
     filter(area %in% c("hbll_s")) # from gfdata HBLL n and south merged
+  grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
   ggplot(grid, aes(lon, lat, colour = area)) +
     geom_point()
   years <- seq(min(d$year), 2023, 1)
   grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
   formula <- catch_count ~ 1
   formuladepth <- catch_count ~ log_botdepth + log_botdepth2
-  }
+}
 
 if (model == "hbll-n") {
   d <- df |>
-    filter(survey_sep %in% c("HBLL INS N")) |> # should I removed survey_abbrev = other?
+    filter(survey_sep %in% c("HBLL INS N")) |> # should survey_abbrev != other?
     drop_na(offset) |>
     drop_na(depth_m) |>
     drop_na(catch_count)
@@ -152,53 +153,79 @@ if (model == "hbll-n") {
 
   grid <- readRDS("output/prediction-grid-hbll-n-s.rds") |>
     filter(area %in% c("hbll_n")) # from gfdata HBLL n and south merged
-  ggplot(grid, aes(lon, lat, colour = area)) +
-    geom_point()
+  grid$log_botdepth2 <- grid$log_botdepth * grid$log_botdepth
   years <- seq(min(d$year), 2023, 1)
   grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
   formula <- catch_count ~ 1
   formuladepth <- catch_count ~ log_botdepth + log_botdepth2
-  }
+}
 
-if (model == "dog-circle") {
+if (model == "dog") {
   d <- df |>
-    filter(survey_sep %in% c("dog comp", "dog")) |>
+    # filter(survey_sep %in% c("dog comp", "dog")) |>
+    filter(survey_sep %in% c("dog comp", "dog", "dog-jhook")) |> #<- couldn't get to converge
     filter(month > 9) # i did this to drop the comp work that happened in summer, keep in and include julian if wanted
-  sort(unique(d$year)) # these are the correct dogfish survey year + 2023
-  sort(unique(d$month))
-  max(d$latitude)
   years <- seq(min(d$year), max(d$year), 1)
   grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
-  grid <- grid |> filter(latitude < 50)
-  formula <- catch_count ~1
-  formuladepth <- catch_count ~ log_botdepth + log_botdepth2 + as.factor(survey_lumped)  x <- ggplot(grid, aes(longitude, latitude)) +
-    geom_point()
-  x + geom_point(data = d, aes(longitude, latitude), colour = "red")
+  grid <- grid |> filter(latitude < max(d$latitude))
+  formula <- catch_count ~ 1 + as.factor(survey_lumped)
+  formuladepth <- catch_count ~ log_botdepth + log_botdepth2 + as.factor(survey_lumped)
 }
 
-if (model == "hblldog_nojhook") {
-  d <- df |>
-    filter(!year %in% c(1986, 1989)) |> #<- dog comp is already rm from db
-    filter(survey_sep != "dog-jhook") # |>
-years <- seq(min(d$year), 2023, 1)
-grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
-formula = catch_count ~ 1 + as.factor(survey_lumped)
-#formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_lumped)
-}
+# if (model == "hblldog_nojhook") {
+#   d <- df |>
+#     filter(!year %in% c(1986, 1989)) |> #<- dog comp is already rm from db
+#     filter(survey_sep != "dog-jhook") # |>
+#   years <- seq(min(d$year), 2023, 1)
+#   grid <- purrr::map_dfr(years, ~ tibble(grid, year = .x))
+#   formula <- catch_count ~ 1 + as.factor(survey_lumped)
+#   # formula = catch_count ~ poly(log_botdepth, 2) + as.factor(survey_lumped)
+# }
 
 # index generation--------
 # create the mesh
-if (model %in% c("hblldog_no2004", "hbll-n-s", "hblldog_nojhook")) {
+if (model %in% c("hblldog_no2004", "hblldog_nojhook")) {
   cutoff <- 3
+} else {
+  if (model %in% c("hbll-n-s", "hblldog_nojhook")) {
+    cutoff <- 3 #<- does this work for the int only model? try 5 if no
+  } else {
+    if (model %in% c("hbll-n")) {
+      cutoff <- 3
+    } else {
+      if (model %in% c("hbll-s")) {
+        cutoff <- 2
+      } else {
+        if (model %in% c("dog")) {
+          cutoff <- 10
+        }
+      }
+    }
+  }
 }
-
-# if (model %in% c("hbll-n", "hbll-s", "hbll-s", "dog-circle") {
-#   cutoff <- 2
-# }
 
 mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = cutoff)
 plot(mesh)
 mesh$mesh$n
+
+#<- should hbll-n be on?
+if (model %in% c("hbll-s", "hbll-n", "dog")) {
+  spatial <- "off"
+} else {
+  spatial <- "on"
+}
+
+if (model %in% c("hbll-n", "hbll-s")) {
+  priors <- sdmTMBpriors(b = normal(c(NA, 0, 0), c(NA, 1, 1))) # no prior on int & survey
+} else {
+  if (model == "dog") { #<- two surveys
+    priors <- sdmTMBpriors(b = normal(c(NA, 0, 0, NA, NA), c(NA, 1, 1, NA, NA)))
+  } else {
+    if (model == "hblldog_no2004") { #<- three surveys
+      priors <- sdmTMBpriors(b = normal(c(NA, 0, 0, NA, NA, NA), c(NA, 1, 1, NA, NA, NA)))
+    }
+  }
+}
 
 ggplot() +
   geom_point(
@@ -242,70 +269,95 @@ fit <- sdmTMB(
   time = "year",
   offset = "offset",
   mesh = mesh,
-  spatial = "on",
+  spatial = spatial,
   spatiotemporal = "rw",
-  family = delta_gamma(),
-  silent = FALSE,
+  family = delta_gamma(), #<- try nbinom2 for dog
+  silent = TRUE,
   share_range = FALSE,
   extra_time = extratime,
-  control = sdmTMBcontrol(newton_loops = 1L)
+  control = sdmTMBcontrol(newton_loops = 1L),
+  # priors = sdmTMBpriors(b = normal(c(0,0), c(1,1))) #<- did this for dog survey
   # do_index = TRUE,
   # predict_args = list(newdata = grid),
   # index_args = list(area = grid$area_km2)
 )
 
-#I  want to run and generate indices for two one with and without depth
-#maybe come back to make the grid smaller for depth to hopefully deal with the centre depth issue better.
-fitdepth <- update(fit, formula = formuladepth)
+fit$sd_report
+sanity(fit)
+AIC(fit)
+
+# generate indices with and w/out depth
+fitdepth <- update(fit, formula = formuladepth) # why doesn't this work?
+
+fitdepth <- sdmTMB(
+  formula = formuladepth,
+  data = d,
+  time = "year",
+  offset = "offset",
+  mesh = mesh,
+  spatial = spatial,
+  spatiotemporal = "rw",
+  family = delta_gamma(), #<- nbinom2 for dog
+  silent = TRUE,
+  # priors = priors, #<- turn off for dog
+  share_range = FALSE,
+  extra_time = extratime,
+  control = sdmTMBcontrol(newton_loops = 1L)
+)
 fitdepth$sd_report
 sanity(fitdepth)
-saveRDS(fitdepth, file = paste0("output/fit-sog-intonly", model, ".rds"))
 AIC(fitdepth)
-AIC()
 
-if(AIC(fit) < AIC(fitdepth)){
+if (AIC(fit) < AIC(fitdepth)) {
   print("intercept model has lower AIC")
+  print(paste0(AIC(fit), " ", AIC(fitdepth)))
+} else {
+  if (AIC(fit) > AIC(fitdepth)) {
+    print("depth model has lower AIC")
+  }
   print(paste0(AIC(fit), " ", AIC(fitdepth)))
 }
 
-#i want this to save only if it converges
-if (!exists("fit")) {
-  fit <- NULL
-}
-fit$sd_report
-sanity(fit)
-saveRDS(fit, file = paste0("output/fit-sog-intonly", model, ".rds"))
-AIC(fit)
-
-if (!exists("fitdepth")) {
-  fitdepth <- NULL
-}
-saveRDS(fitdepth, file = paste0("output/fit-sog-depth", model, ".rds"))
-
-#check which model fit is better
 ok <- all(unlist(sanity(fit)))
-if (!ok) {
-  try({
-    fitnb <- sdmTMB(
-      formula = formula,
-      data = d,
-      time = "year",
-      offset = "offset",
-      mesh = mesh,
-      spatial = "on",
-      spatiotemporal = "rw",
-      family = nbinom2(),
-      silent = FALSE,
-      share_range = FALSE,
-      extra_time = extratime,
-      control = sdmTMBcontrol(newton_loops = 1L)
-    )
-  })
+if (!ok) { # does this save it if it converged?
+  # if (!exists("fit")) {
+  fit <- NULL
+} else {
+  saveRDS(fit, file = paste0("output/fit-sog-intonly", model, ".rds"))
 }
-AIC(fit)
-AIC(fitnb)
 
-#I want this to check the AIC values between delta gamma and nbinom
+ok <- all(unlist(sanity(fitdepth)))
+if (!ok) { # does this save it if it converged?
+  # if (!exists("fitdepth")) {
+  fitdepth <- NULL
+} else {
+  saveRDS(fitdepth, file = paste0("output/fit-sog-depth", model, ".rds"))
+}
+
+# check if NB mdoel fit is better
+# ok <- all(unlist(sanity(fit)))
+# if (!ok) {
+#   try({
+# fitnb <- sdmTMB(
+#   formula = formula,
+#   data = d,
+#   time = "year",
+#   offset = "offset",
+#   mesh = mesh,
+#   spatial = "off",
+#   spatiotemporal = "rw",
+#   family = nbinom2(),
+#   silent = TRUE,
+#   share_range = FALSE,
+#   extra_time = extratime,
+#   control = sdmTMBcontrol(newton_loops = 1L)
+# )
+#   })
+# }
+# AIC(fit)
+# AIC(fitnb)
+
+# I want this to check the AIC values between delta gamma and nbinom
 # if (!exists(fitnb)) {
 #     fitnb <- NULL
 #   }
@@ -315,11 +367,37 @@ AIC(fitnb)
 # }
 
 # get index
-# I want this to run over both the fit and fitdepth models
-pred <- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
-index <- get_index(pred, bias_correct = TRUE)
-index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
-saveRDS(index, file = paste0("output/ind-sog-intonly", model, ".rds"))
+if (!is.null(fit)) { #<- #if fit is null ignore this
+  pred <- predict(fit, grid, return_tmb_object = TRUE, response = TRUE)
+  index <- get_index(pred, bias_correct = TRUE)
+  index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
+  index$modelloc <- model
+  index$type <- "int"
+  saveRDS(index, file = paste0("output/ind-sog-intonly", model, ".rds"))
+}
+
+if (!is.null(fitdepth)) {
+  grid$survey_lumped <- "dog"
+  pred <- predict(fitdepth, grid, return_tmb_object = TRUE, response = TRUE)
+  index <- get_index(pred, bias_correct = TRUE)
+  index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
+  index$modelloc <- model
+  index$type <- "depth"
+  saveRDS(index, file = paste0("output/ind-sog-depth", model, ".rds"))
+}
+
+# high res grid, runs out of memory so chunked it up
+# if (!is.null(fitdepth)){
+#   pred <- predict(fitdepth, filter(grid, year %in% c(2003:2013)), return_tmb_object = TRUE, response = TRUE)
+#   index <- get_index(pred, bias_correct = TRUE)
+#   pred2 <- predict(fitdepth, filter(grid, year %in% c(2014:2023)), return_tmb_object = TRUE, response = TRUE)
+#   index2 <- get_index(pred2, bias_correct = TRUE)
+#   index <- bind_rows(index, index2)
+#   index <- index |> mutate(model = ifelse(year %in% unique(d$year), "yrs_surved", "yrs_interp"))
+#   index$modelloc <- model
+#   index$type <- "depth-highres"
+#   saveRDS(index, file = paste0("output/ind-sog-depth-", model, "-highres.rds"))
+# }
 
 # ggplot
 if (exists("index")) {
@@ -344,5 +422,3 @@ if (exists("index")) {
   #     scale_x_continuous(breaks = c(yearlabs)) +
   #     theme(axis.text.x = element_text(angle = 45, hjust = 1))
 }
-
-
