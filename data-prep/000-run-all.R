5f42420c8b83f55936904fcb7098224a671887aa
l digity <ldigity@MacBook-Pro-5.local>
2024-12-07 06:31:36 -0800

    init commit

---
+++ data-prep/000-run-all.R
@@ -0,0 +1,20 @@
+#index file for data-prep
+
+01-pull_data.R #<- do I need this anymore?
+01B-pull-data-get_all.R
+
+02a-data-clean-sets
+02b-data-clean-samples.R
+
+03-wrangle-hbll-dog-sets.R
+03a-wrangle-hbll-dog-samples.R
+
+05-comparative-wrk-summaries.R
+05a-comparative-work-sample-summaries.R
+
+04-summary-set-plots.R
+04a-summary-samples-plots.R
+#not run
+01-pull_data-sql-code #<- sql code for pulling Dogfish from gfbio
+02-data-clean.R #<-erase? now in the other two cleaning files?
+99-diff-between-getall-sql
