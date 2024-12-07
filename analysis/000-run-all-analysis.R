d48c06ade4aba4b552f109c5d44cdef9d4c0dd3c
l digity <ldigity@MacBook-Pro-5.local>
2024-12-07 06:42:53 -0800

    init commit

---
+++ analysis/000-run-all-analysis.R
@@ -0,0 +1,26 @@
+#index file for analysis
+
+01-prediction-grid-generation.R
+
+01-summary-stats.R # i think this is in data-prep now, remove?
+
+02-index-generation.R
+02-lengthscomps_hbllDOG.R #<- not sure i use this anymore
+02a-index-generation-julian-date.R #<- senstivity with julian to see how it affects index
+02b-index-generation-time-varying-depth.R #not sure
+02c-index-generation-maturity-grouping.R #<- don't think I used this
+02d-dog-desgnbased-time-varying-depth #<- not usre
+
+03-catch-composition.R #not sure
+03-catch-species-composition.R #to calc the freq of species caught on Dgofish versue HBLL
+
+03-index-figs.R #not usre
+03-raw-summary-figs #maybe some figures
+03-summarysummaries.R #not sure
+
+03a-temporal_lengths_comps #i think i use
+03b-temporal_depth_relationship.R #not sure
+
+04-lenth-comparisons.R
+
+DogfishTechReport.R # dont think i use
