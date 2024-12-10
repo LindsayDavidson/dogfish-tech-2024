c52f04b405b2267f18e73a349f5690bec0d672b1
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-12-10 10:37:04 -0800

    clean

--- data-prep/01-pull-data.R
+++ data-prep/01-pull-data.R
@@ -1,8 +1,6 @@
-# pull dogfish data using the new function *get_all* in gfbio
-# there is a branch on PBS assess that has this code.
-# https://github.com/pbs-assess/Dogfish-survey/blob/main/Dogfish_data_pull.R
+# pull dogfish data
+#see 999-pull_data-sql-code for pulling code directly from gfbio
 
-# Code for creating one database of all Dogfish surveys including comparisons, j hooks, and dogfish surveys
 # Note
 # SURVEY_SERIES_ID == 48) #comp work 2004, 2019, 2022, 2023
 # SURVEY_SERIES_ID == 93) #circle hook dog surveys 2005 onwards (2005, 2008, 2011, 2014, 2019)
@@ -15,16 +13,16 @@
 # 2022 comparison work has two gear types per set
 # 2023 comparison work has two gear types per set  and was completed during the HBLL and DOG survey
 
-# library -----------------------------------------------------------------
 library(tidyverse)
 library(sf)
 library(ggplot2)
 library(here)
 library(sp)
 library(gfdata)
-remotes::install_github("pbs-assess/gfdata", ref = "trials", force = TRUE)
+#remotes::install_github("pbs-assess/gfdata", ref = "trials", force = TRUE)
 
 # pull inside sets data ------------------------------------------------
+#survey ids include all of the dogfish surveys on the inside
 d <- get_all_survey_sets("north pacific spiny dogfish", ssid = c(76, 48, 39, 40))
 unique(d$survey_abbrev)
 saveRDS(d, "data-raw/dogfish_sets_getall.rds")
@@ -47,7 +45,6 @@ saveRDS(d, "data-raw/dogfish_samples_gfdata.rds")
 
 
 # pull all species for each survey ----------------------------------------
-
 catch_dogfish <- gfdata::run_sql("GFBioSQL", "SELECT
 FEC.FISHING_EVENT_ID,
 FE.FE_PARENT_EVENT_ID,
