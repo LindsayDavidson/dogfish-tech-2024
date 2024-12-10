300670488b2b1a35a730bcbcdfdd8654d008abe1
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2024-12-10 13:57:26 -0800

    clean

--- analysis/000-run-all-analysis.R
+++ analysis/000-run-all-analysis.R
@@ -1,25 +1,20 @@
 #index file for analysis
-999-prediction-grid-generation.R
 
-01-summary-stats.R # i think this is in data-prep now, remove?
+01-index-generation.R
+01a-index-generation-julian-date.R #<- sensitivity with julian to see how it affects index
+01b-index-generation-time-varying-depth.R #yes, hbll depth trends through time
+01c-index-generation-maturity-grouping.R #<- don't think I used this
+01d-dog-designbased-time-varying-depth #<- not used?
 
-02-index-generation.R
-02-lengthscomps_hbllDOG.R #<- not sure i use this anymore
-02a-index-generation-julian-date.R #<- senstivity with julian to see how it affects index
-02b-index-generation-time-varying-depth.R #not sure
-02c-index-generation-maturity-grouping.R #<- don't think I used this
-02d-dog-desgnbased-time-varying-depth #<- not usre
+02-catch-species-composition.R #to calc the freq of species caught on Dogfish versus HBLL
 
-03-catch-composition.R #not sure
-03-catch-species-composition.R #to calc the freq of species caught on Dgofish versue HBLL
-
-03-index-figs.R #not usre
-03-raw-summary-figs #maybe some figures
-03-summarysummaries.R #not sure
-
-03a-temporal_lengths_comps #i think i use
+03-index-figs.R #yes index plots
+03-survey-variability-plots.R #yes some som figures
+03a-lengths-comp-figure-temporal-trends.R #yes SOM length comps through time plot
 03b-temporal_depth_relationship.R #not sure
-
-04-lenth-comparisons.R
-
+04-length-comparisons.R #not sure
 DogfishTechReport.R # dont think i use
+
+999-prediction-grid-generation.R
+999-load-grid-data.R #<- load the grid for hbll n, s, and dog
+999-load-hbll-n-s-grid.R #<- load the grid for hbll n, s
