e0296942e9c6a9409927841fae810eb851ee806f
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-04-17 11:28:36 -0700

    cleaner season code and pull missing dates from sets data

--- data-prep/02b-data-clean-samples.R
+++ data-prep/02b-data-clean-samples.R
@@ -3,10 +3,21 @@ library(sf)
 library(ggplot2)
 library(tidyverse)
 library(sp)
+library(lubridate)
+
+spring <- seq(12, 25, 1)
+summer <- seq(26, 38, 1)
+winter <- c(seq(1, 11, 1), 52)
+fall <- seq(39, 51, 1)
 
 # load data ---------------------------------------------------------------
 
 samps <- readRDS("data-raw/dogfish_samples_getall.rds")
+sets <- readRDS("data-generated/dogfish_sets_cleaned_getall.rds") |>
+  dplyr::select(year, fishing_event_id, week) |>
+  distinct()
+samps <- left_join(samps, sets)
+samps |> filter(is.na(week) == TRUE) |> tally()
 
 samplesf <- samps |>
   mutate(survey_sep = case_when(
@@ -45,40 +56,17 @@ samplesf <- samps |>
     year == 2004 & hooksize_desc == "12/0" & survey_abbrev == "OTHER" ~ "dog-jhook"
   ))
 
-samplesf <- samplesf |>
-  mutate(survey_timing = case_when(
-    survey_abbrev == "HBLL INS S" ~ "summer",
-    survey_abbrev == "HBLL INS N" ~ "summer",
-    year %in% c(1986, 1989) ~ "fall",
-    year %in% c(2005, 2008, 2011, 2014) & survey_abbrev == "DOG" ~ "fall",
-    year == 2019 & survey_abbrev == "DOG" ~ "fall",
-    year == 2019 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "13/0" ~ "summer",
-    year == 2019 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "14/0" ~ "summer",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "12/0" ~ "fall",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "14/0" & month == 9 & day >= 27 ~ "fall",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "13/0" & month == 9 & day >= 27 ~ "fall",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "13/0" & month == 9 & day < 27 ~ "summer",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "14/0" & month == 9 & day < 27 ~ "summer",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "13/0" & month == 8 ~ "summer",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "14/0" & month == 8 ~ "summer",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "13/0" & month == 10 ~ "fall",
-    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "14/0" & month == 10 ~ "fall",
-    year == 2022 & hooksize_desc == "13/0" & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" ~ "summer",
-    year == 2022 & hooksize_desc == "14/0" & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" ~ "summer",
-    year == 2004 & hooksize_desc == "14/0" & survey_abbrev == "OTHER" ~ "fall",
-    year == 2004 & hooksize_desc == "12/0" & survey_abbrev == "OTHER" ~ "fall"
-  ))
+
+#add season based on week of survey and definitions above
+final <- final |>
+  mutate(season = ifelse(week %in% spring, "2", ifelse(week %in% summer, "3", ifelse(week %in% fall, "4", "1"))))
 
 # so I can put the different surveys into a model and account for julian date after (the seasonal component of the comparison work)
 samplesf <- samplesf |>
   mutate(survey_lumped = case_when(
-    survey_sep == "HBLL INS S" ~ "hbll",
-    survey_sep == "HBLL INS N" ~ "hbll",
+    survey_sep %in% c("HBLL INS S", "HBLL INS N", "hbll", "hbll comp") ~ "hbll",
     survey_sep == "dog-jhook" ~ "dog-jhook",
-    survey_sep == "dog" ~ "dog",
-    survey_sep == "hbll comp" ~ "hbll",
-    survey_sep == "dog comp" ~ "dog",
-    survey_sep == "hbll" ~ "hbll"
+    survey_sep %in% c("dog", "dog comp") ~ "dog"
   ))
 
 
