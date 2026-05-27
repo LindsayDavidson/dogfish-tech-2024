9f13e5558a6f6f27c8ad69cf24f157dad93eb176
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-05-27 15:53:48 -0700

    add unique id

--- data-prep/02b-data-clean-samples.R
+++ data-prep/02b-data-clean-samples.R
@@ -56,9 +56,45 @@ samplesf <- samps |>
     year == 2004 & hooksize_desc == "12/0" & survey_abbrev == "OTHER" ~ "dog-jhook"
   ))
 
+samplesf <- samplesf |>
+  mutate(survey_timing = case_when(
+    survey_abbrev == "HBLL INS S" ~ "HBLL INS S",
+    survey_abbrev == "HBLL INS N" ~ "HBLL INS N",
+    year %in% c(1986, 1989) ~ "dog-jhook",
+    year %in% c(2005, 2008, 2011, 2014) & survey_abbrev == "DOG" ~ "dog",
+    year == 2019 & survey_abbrev == "DOG" ~ "dog",
+    year == 2019 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "13/0" ~ "hbll",
+    year == 2019 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "14/0" ~ "hbll",
+    # year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "14/0" ~ "dog",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "12/0" ~ "dog-jhook",
+    # year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" & hooksize_desc == "13/0" ~ "hbll comp",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "14/0" & month == 9 & day >= 27 ~ "dog",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "13/0" & month == 9 & day >= 27 ~ "dog",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "13/0" & month == 9 & day < 27 ~ "hbll",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "14/0" & month == 9 & day < 27 ~ "hbll",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "13/0" & month == 8 ~ "hbll",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "14/0" & month == 8 ~ "hbll",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "13/0" & month == 10 ~ "dog",
+    year == 2023 & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" &
+      hooksize_desc == "14/0" & month == 10 ~ "dog",
+    year == 2022 & hooksize_desc == "13/0" & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" ~ "hbll",
+    year == 2022 & hooksize_desc == "14/0" & activity_desc == "DOGFISH GEAR/TIMING COMPARISON SURVEYS" ~ "hbll",
+    year == 2004 & hooksize_desc == "14/0" & survey_abbrev == "OTHER" ~ "dog comp",
+    year == 2004 & hooksize_desc == "12/0" & survey_abbrev == "OTHER" ~ "dog-jhook"
+  ))
 
 #add season based on week of survey and definitions above
-final <- final |>
+samplesf <- samplesf |>
   mutate(season = ifelse(week %in% spring, "2", ifelse(week %in% summer, "3", ifelse(week %in% fall, "4", "1"))))
 
 # so I can put the different surveys into a model and account for julian date after (the seasonal component of the comparison work)
