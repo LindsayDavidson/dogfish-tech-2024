974cf08eccdb3db7d13a0e0b2ca4854edc2e5730
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-06-02 16:12:15 -0700

    init commit

--- analysis/01b-index-generation-time-varying-depth.R
+++ analysis/01b-index-generation-time-varying-depth.R
@@ -1,11 +1,8 @@
 library(sdmTMB)
 library(ggplot2)
 library(tidyverse)
+remotes::install_github("pbs-assess/sdmTMB@dev", force = TRUE)
 library(sdmTMB)
-
-latitude_cutoff <- 50.34056
-bccrs <- 32609
-source("999-load-hbll-n-s-grid.R") #need a different grid, see below
 
 df <- readRDS("data-raw/wrangled-hbll-dog-sets.rds") |>
   drop_na(catch_count) |>
@@ -24,176 +21,199 @@ df <- df |>
     depth_m > 165 & depth_m <= 220  ~ 4,
     depth_m > 220 ~ 5 ))
 
-# hbll n and s only -------------------------------------------------------------
-d <- df |>
-  #filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N")) |>
-  filter(survey_lumped == "hbll") |>
-  drop_na(offset) |>
-  drop_na(depth_m) |>
-  drop_na(julian) |>
-  drop_na(catch_count)
-range(d$depth_m)
-df <- df |> #4 points are deeper than 110, instead of dropping ill change the categorizations
-  mutate(depth_bin = case_when(
-    depth_m <= 70 ~ 1,
-    depth_m > 70 & depth_m <= 150 ~ 2))
-
-
-
-
-# shallow deep hbll block model ------------------------------------------------------
-d$year_factor <- as.factor(d$year)
-mean <- mean(d$log_botdepth)
-d$log_botdepth_c <- d$log_botdepth - mean
-d$log_botdepth_c2 <- d$log_botdepth_c * d$log_botdepth_c
-unique(d$depth_bin)
-
-dsh <- filter(d, depth_bin == 1)
-gridshallow <- filter(grid, depth_bin == 1)
-meshsh <- make_mesh(dsh, c("UTM.lon", "UTM.lat"), cutoff = 15)
-plot(meshsh)
-
-ddp <- filter(d, depth_bin == 2)
-griddeep <- filter(grid, depth_bin == 2)
-meshdp <- make_mesh(ddp, c("UTM.lon", "UTM.lat"), cutoff = 15)
-plot(meshdp)
-
-ms <- sdmTMB(
-  formula = catch_count ~ 1,
-  offset = "offset",
-  time = "year",
-  spatiotemporal = "rw",
-  silent = FALSE,
-  spatial = "on",
-  family = delta_lognormal(),
-  mesh = meshsh,
-  data = dsh,
-  do_index = FALSE,
-  extra_time = c(2006, 2017, 2020)
-)
-saveRDS(ms, "output/fit-depth-bins-shallow.rds")
-ms <- readRDS("output/fit-depth-bins-shallow.rds")
-
-md <- sdmTMB(
-  formula = catch_count ~ 1,
-  offset = "offset",
-  time = "year",
-  spatiotemporal = "rw",
-  silent = FALSE,
-  spatial = "off",
-  family = delta_gamma(),
-  mesh = meshdp,
-  data = ddp,
-  do_index = FALSE,
-  extra_time = c(2006, 2017, 2020)
-)
-saveRDS(md, "output/fit-depth-bins-deep.rds")
-md <- readRDS("output/fit-depth-bins-deep.rds")
-
-sanity(ms)
-sanity(md)
-ms$sd_report
-md$sd_report
-
-
-psh <- predict(ms, newdata = gridshallow, se_fit = TRUE, re_form = NA, return_tmb_object = TRUE)
-index <- get_index(psh, bias_correct = TRUE)
-index$loc <- "shallow"
-saveRDS(psh, paste0("output/ind-depth-bin-shallow", model, ".rds"))
-
-pdeep <- predict(md, newdata = griddeep, response = TRUE, return_tmb_object = TRUE)
-index_d <- get_index(pdeep, bias_correct = TRUE)
-index_d$loc <- "deep"
-saveRDS(index_d, paste0("output/ind-depth-bin-deep", model, ".rds"))
-
-index <- bind_rows(index, index_d)
-index |>
-  #group_by(year) |>
-  #mutate(est2 = est - est[depth_m == min(depth_m)]) |>
-  mutate(est2 = est - mean(est)) |>
-  ggplot() +
-  geom_line(aes(year, (est), group = loc, colour = loc)) +
-  #geom_pointrange(mapping = aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6, position = position_dodge(width = 0.5)) +
-  scale_colour_viridis_d() +
-  theme_classic()
-
-ggplot(index, aes(year, (est), ymin = (lwr), ymax = (upr), group = loc, colour = loc)) +
-  geom_pointrange(mapping = aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6, position = position_dodge(width = 0.5)) +
-  scale_colour_manual(values = c("#d8b365", "#5ab4ac")) +
-  theme_classic() +
-  ylab("Abundance estimate") +
-  xlab("Year")  +
-  coord_cartesian(ylim = c(0,50))
-ggsave("Figures/depth-bin-figure.jpg", width = 4, height = 3)
-
-ggplot(index, aes(year, (est), ymin = (lwr), ymax = (upr), group = loc, colour = loc)) +
-  geom_pointrange(data = index, mapping = aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6, position = position_dodge(width = 0.5)) +
-  theme_classic() +
-  coord_cartesian(ylim = c(0, 30)) +
-  scale_colour_manual(values = c("grey", "black"))
-ggsave("Figures/ind_deep_shallow.jpg", width = 4, height = 3)
-
-p %>%
-  group_by(year) %>%
-  summarize(max_est = max(est), xint = (depth_m[est == max_est])) |>
-  # filter(year > 2003) |>
-  ggplot() +
-  theme_classic() +
-  geom_line(aes(year, xint, colour = year, group = year)) +
-  geom_point(aes(year, xint, colour = year, group = year))
-
-ggplot(p, aes(depth_m, exp(est),
-              ymin = exp(est - 1.96 * est_se),
-              ymax = exp(est + 1.96 * est_se),
-              group = as.factor(year)
-)) +
-  geom_line(aes(colour = year), lwd = 1) +
-  geom_ribbon(aes(fill = year), alpha = 0.1) +
-  scale_colour_viridis_c() +
-  scale_fill_viridis_c() +
-  coord_cartesian(expand = F) +
-  labs(x = "Depth (m)", y = "Density (number/km2)") +
-  theme_classic()
-ggsave("Figures/fit-tv-predicteddepth_CIs.jpg", width = 4, height = 3)
+# # hbll n and s only -------------------------------------------------------------
+# d <- df |>
+#   filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N")) |>
+#   #filter(survey_lumped == "hbll") |>
+#   drop_na(offset) |>
+#   drop_na(depth_m) |>
+#   drop_na(julian) |>
+#   drop_na(catch_count)
+# range(d$depth_m)
+# df <- df |> #4 points are deeper than 110, instead of dropping ill change the categorizations
+#   mutate(depth_bin = case_when(
+#     depth_m <= 70 ~ 1,
+#     depth_m > 70 & depth_m <= 150 ~ 2))
+#
+# family = betabinomial(link = "cloglog")
+#
+# weights <- (d$lglsp_hook_count * d$soak)
+#
+#
+#
+#
+# # shallow deep hbll block model ------------------------------------------------------
+# d$year_factor <- as.factor(d$year)
+#
+# #mean <- mean(d$log_botdepth)
+# #d$log_botdepth_c <- d$log_botdepth - mean
+# #d$log_botdepth_c2 <- d$log_botdepth_c * d$log_botdepth_c
+# #unique(d$depth_bin)
+# #dsh <- filter(d, depth_bin == 1)
+#
+# source("analysis/999-load-hbll-n-s-grid.R") #need a different grid, see below
+# #gridshallow <- filter(grid, depth_bin == 1)
+#
+# meshsh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 15)
+# plot(meshsh)
+#
+# #ddp <- filter(d, depth_bin == 2)
+# #griddeep <- filter(grid, depth_bin == 2)
+#
+# meshdp <- make_mesh(ddp, c("UTM.lon", "UTM.lat"), cutoff = 10)
+# plot(meshdp)
+#
+# ms <- sdmTMB(
+#   formula = catch_count ~ 1,
+#   offset = "offset",
+#   time = "year",
+#   spatiotemporal = "rw",
+#   weights = weights,
+#   silent = FALSE,
+#   spatial = "on",
+#   family = betabinomial("cloglog"),
+#   mesh = meshsh,
+#   data = dsh,
+#   do_index = FALSE,
+#   extra_time = c(2006, 2017, 2020)
+# )
+#
+# saveRDS(ms, "output/fit-depth-bins-shallow.rds")
+# ms <- readRDS("output/fit-depth-bins-shallow.rds")
+#
+# md <- sdmTMB(
+#   formula = catch_count ~ 1,
+#   offset = "offset",
+#   time = "year",
+#   spatiotemporal = "rw",
+#   silent = FALSE,
+#   spatial = "off",
+#   family = delta_gamma(),
+#   mesh = meshdp,
+#   data = ddp,
+#   do_index = FALSE,
+#   extra_time = c(2006, 2017, 2020)
+# )
+# saveRDS(md, "output/fit-depth-bins-deep.rds")
+# md <- readRDS("output/fit-depth-bins-deep.rds")
+#
+# sanity(ms)
+# sanity(md)
+# ms$sd_report
+# md$sd_report
+#
+#
+# psh <- predict(ms, newdata = gridshallow, se_fit = TRUE, re_form = NA, return_tmb_object = TRUE)
+# index <- get_index(psh, bias_correct = TRUE)
+# index$loc <- "shallow"
+# saveRDS(psh, paste0("output/ind-depth-bin-shallow", model, ".rds"))
+#
+# pdeep <- predict(md, newdata = griddeep, response = TRUE, return_tmb_object = TRUE)
+# index_d <- get_index(pdeep, bias_correct = TRUE)
+# index_d$loc <- "deep"
+# saveRDS(index_d, paste0("output/ind-depth-bin-deep", model, ".rds"))
+#
+# index <- bind_rows(index, index_d)
+# index |>
+#   #group_by(year) |>
+#   #mutate(est2 = est - est[depth_m == min(depth_m)]) |>
+#   mutate(est2 = est - mean(est)) |>
+#   ggplot() +
+#   geom_line(aes(year, (est), group = loc, colour = loc)) +
+#   #geom_pointrange(mapping = aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6, position = position_dodge(width = 0.5)) +
+#   scale_colour_viridis_d() +
+#   theme_classic()
+#
+# ggplot(index, aes(year, (est), ymin = (lwr), ymax = (upr), group = loc, colour = loc)) +
+#   geom_pointrange(mapping = aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6, position = position_dodge(width = 0.5)) +
+#   scale_colour_manual(values = c("#d8b365", "#5ab4ac")) +
+#   theme_classic() +
+#   ylab("Abundance estimate") +
+#   xlab("Year")  +
+#   coord_cartesian(ylim = c(0,50))
+# ggsave("Figures/depth-bin-figure.jpg", width = 4, height = 3)
+#
+# ggplot(index, aes(year, (est), ymin = (lwr), ymax = (upr), group = loc, colour = loc)) +
+#   geom_pointrange(data = index, mapping = aes(x = year - 0.25), size = 0.2, pch = 5, alpha = 0.6, position = position_dodge(width = 0.5)) +
+#   theme_classic() +
+#   coord_cartesian(ylim = c(0, 30)) +
+#   scale_colour_manual(values = c("grey", "black"))
+# ggsave("Figures/ind_deep_shallow.jpg", width = 4, height = 3)
+#
+# p %>%
+#   group_by(year) %>%
+#   summarize(max_est = max(est), xint = (depth_m[est == max_est])) |>
+#   # filter(year > 2003) |>
+#   ggplot() +
+#   theme_classic() +
+#   geom_line(aes(year, xint, colour = year, group = year)) +
+#   geom_point(aes(year, xint, colour = year, group = year))
+#
+# ggplot(p, aes(depth_m, exp(est),
+#               ymin = exp(est - 1.96 * est_se),
+#               ymax = exp(est + 1.96 * est_se),
+#               group = as.factor(year)
+# )) +
+#   geom_line(aes(colour = year), lwd = 1) +
+#   geom_ribbon(aes(fill = year), alpha = 0.1) +
+#   scale_colour_viridis_c() +
+#   scale_fill_viridis_c() +
+#   coord_cartesian(expand = F) +
+#   labs(x = "Depth (m)", y = "Density (number/km2)") +
+#   theme_classic()
+# ggsave("Figures/fit-tv-predicteddepth_CIs.jpg", width = 4, height = 3)
 
 # time varying model ------------------------------------------------------
 
-mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 15)
+d <- df |>
+    filter(survey_abbrev %in% c("HBLL INS S", "HBLL INS N")) |>
+    #filter(survey_lumped == "hbll") |>
+    drop_na(offset) |>
+    drop_na(depth_m) |>
+    drop_na(julian) |>
+    drop_na(catch_count)
+d$catch_prop <- d$catch_count/d$lglsp_hook_count
+weights <- (d$lglsp_hook_count * d$soak)
+range(weights)
+
+mesh <- make_mesh(d, c("UTM.lon", "UTM.lat"), cutoff = 10)
 plot(mesh)
 
 d$year_factor <- as.factor(d$year)
 mean <- mean(d$log_botdepth)
 d$log_botdepth_c <- d$log_botdepth - mean
 d$log_botdepth_c2 <- d$log_botdepth_c * d$log_botdepth_c
+unique(d$year_factor)
+
 
 fittv <- sdmTMB(
-  formula = catch_count ~ 0 + year_factor, #year here to soak up the year to year variation otherwise it all goes into the depth relationship
-  offset = "offset",
+  formula = catch_prop ~ 0 + year_factor, #year here to soak up the year to year variation otherwise it all goes into the depth relationship
   time = "year",
   time_varying = ~ 1 + log_botdepth_c + log_botdepth_c2, #should this be one to have a time varying intercept for each year not much difference when 0 or 1
+  #time_varying = ~ 1 + poly(log_botdepth_c, 2), #should this be one to have a time varying intercept for each year not much difference when 0 or 1
   time_varying_type = "rw0",
   spatiotemporal = "AR1", #off or AR1 left over variation from the time varying process is ar1
   silent = FALSE,
-  spatial = "on",
-  family = nbinom2(), #<- couldnt get delta_gamma to converge for now
+  spatial = "off",
+  weights = weights,
+  family = betabinomial("cloglog"),
   mesh = mesh,
   data = d,
   do_index = FALSE,
   extra_time = c(2006, 2017, 2020),
   priors = sdmTMBpriors(
-  b = normal(c(0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
-    c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)),
-  matern_st = pc_matern(range_gt = cutoff * 3, sigma_lt = 2),
-  matern_s = pc_matern(range_gt = cutoff * 2, sigma_lt = 1))
-  # control = sdmTMBcontrol(
-  #   start = list(
-  #   ln_tau_V = matrix(log(0.1), 2, 2)
-  #   ),
-  #   map = list(
-  #     ln_tau_V = factor(as.vector(matrix(c(1, NA, NA, 2, NA, NA), 2, 2)))
-  #   ),
-  #   newton_loops = 1L
-  # )
+  #b = normal(c(0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),
+  #  c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,1,1))))
+  matern_st = pc_matern(range_gt = cutoff * 3, sigma_lt = 2)),
+  #matern_s = pc_matern(range_gt = cutoff * 2, sigma_lt = 1))
+  control = sdmTMBcontrol(
+     start = list(
+     ln_tau_V = matrix(log(0.2), 2, 2)
+     ),
+     map = list(
+       ln_tau_V = factor(as.vector(matrix(c(1, NA, 2, NA) , 2, 2)))
+     )
+   )
 )
 
 sanity(fittv)
---
+++ data-prep/999-aging-data.R
@@ -0,0 +1,15 @@
+samps <- readRDS("data-raw/dogfish_samples_cleaned.rds")
+glimpse(samps)
+unique(samps$age_specimen_collected)
+test <- filter(samps, age_specimen_collected == 1)
+ggplot(test, aes(year, length, group = survey_abbrev, colour = survey_abbrev)) + geom_jitter()
+unique(test$year)
+
+
+d <- readRDS("data-raw/dogfish_samples_gfdata.rds")
+unique(d$survey_abbrev)
+unique(d$year)
+unique(d$age) #no dogfish ages in database
+d2 <- filter(d, age_specimen_collected == 1)
+ggplot(d2, aes(year, age, group = survey_abbrev, colour = survey_abbrev)) + geom_jitter()
+unique(test$year)
