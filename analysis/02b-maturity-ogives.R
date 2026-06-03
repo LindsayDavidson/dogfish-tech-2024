10ccebcb2f87ee08898dabc5a67fd651961b5454
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-06-03 13:13:45 -0700

    init commit

--- analysis/02b-maturity-ogives.R
+++ analysis/02b-maturity-ogives.R
@@ -99,3 +99,106 @@ mat3 <- m23$pred_data |> mutate(year = 2023)
 cowplot::plot_grid(x, y, z, labels = c('A. 1986', 'B. 2008', "C. 2023"), label_size = 12, ncol = 1)
 ggsave("Figures/maturity-ogives-sog-temporal.jpg", width = 4, height = 6)
 
+
+
+#test figure
+#m <- readRDS("output/survey_samples_codedmaturity.rds")
+m86
+m08
+m23
+
+raw <- m86$data
+m$data <- m86$data |> mutate(mature_num = ifelse(mature == "FALSE", 0.5, 0.5))
+#m$pred_data <- filter(m$pred_data, female == 1)  #<- plot females only
+nd_re <- m$pred_data
+n_re <- length(unique(nd_re$sample_id)) / 5
+n_re2 <- ifelse(n_re < 15, 15, n_re)
+
+ann_text <- data.frame(
+  age_or_length = c(40, 40, 40, 40), glmm_re = c(0.80, 0.88, 0.60, 0.68),
+  lab = c("F05 = 77.0", "F95 = 95.6", "M05 = 65.1", "M95 = 76.7"),
+  female = factor(c("1", "1", "0", "0"), levels = c("0", "1"))
+)
+
+# string <- data.frame(
+#   length = c(
+#     as.numeric(fm$age_or_length), as.numeric(fi$age_or_length) + 1,
+#     as.numeric(mi$age_or_length), as.numeric(mm$age_or_length - 1)
+#   ),
+#   female = c(1, 1, 0, 0)
+# )
+
+# string <- data.frame(
+#   length = c(
+#     as.numeric(fm$age_or_length), as.numeric(fi$age_or_length) + 1,
+#     as.numeric(mi$age_or_length), as.numeric(mm$age_or_length - 1)
+#   ),
+#   female = c(1, 1, 0, 0)
+# )
+
+p <-
+  ggplot() +
+  geom_line(
+    data = nd_re, aes(age_or_length,
+                      as.numeric(glmm_re),
+                      group = paste(sample_id, female),
+                      #fill = as.character(female),
+                      colour = as.character(female)
+                      #linetype = as.character(female)
+    ),
+    #colour = c("red", "grey"),
+    inherit.aes = FALSE,
+    alpha = 1 / n_re2,
+    show.legend = FALSE
+  ) +
+  # scale_linetype_manual(values = c("dotdash", "solid")) +
+  scale_colour_manual(values = c("grey10", "grey50")) +
+  scale_x_continuous(limits = c(0,125)) +
+  geom_rug(
+    data = filter(m$data, mature == "FALSE"), aes(
+      x = age_or_length,
+      y = mature_num
+      #colour = "female"
+      #linetype = as.character(female)
+    ),
+    #colour = c("red", "grey"),
+    sides = "b",
+    length = unit(c(0.04), "npc"),
+    alpha = c(0.05),
+    lty = 1,
+    show.legend = FALSE
+  ) +
+  geom_rug(
+    data = filter(m$data, mature == "TRUE"), aes(
+      x = age_or_length,
+      y =mature_num
+      #colour = "female"
+      #linetype = as.character(female)
+    ),
+    #colour = c("red", "grey"),
+    sides = "t",
+    alpha = 0.05,
+    # lty = 1,
+    show.legend = FALSE
+  ) +
+  labs(x = "Length (cm)", y = "Probability mature") +
+  theme(plot.margin = unit(c(5, 1, 1, 1), "lines")) +
+  # geom_vline(
+  #   data = string,
+  #   aes(
+  #     xintercept = length,
+  #     #colour = female
+  #     #linetype = as.character(female)
+  #   ),
+  #   colour = c("grey10", "grey10", "grey50", "grey50"),
+  #   show.legend = FALSE,
+  # ) +
+  #facet_wrap(~female, nrow = 1) +
+  theme(strip.text.x = element_blank()) +
+  theme_classic()
+
+p <- p + geom_text(data = ann_text, aes(age_or_length, glmm_re, label = lab),
+                   colour = c("grey10", "grey10", "grey10", "grey10"),
+                   show.legend = FALSE)
+p
+g_mat <- p
