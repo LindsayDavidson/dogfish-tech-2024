8a0d19aec4cf3c53e051638e69d7d8409d4fbd9f
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-06-09 11:58:56 -0700

    age structure

--- data-prep/999-aging-data.R
+++ data-prep/999-aging-data.R
@@ -9,7 +9,7 @@ unique(test$year)
 d <- readRDS("data-raw/dogfish_samples_gfdata.rds")
 unique(d$survey_abbrev)
 unique(d$year)
-unique(d$age) #no dogfish ages in database
+unique(d$age_specimen_collected) #no dogfish ages in database despite having structures
 d2 <- filter(d, age_specimen_collected == 1)
 ggplot(d2, aes(year, age, group = survey_abbrev, colour = survey_abbrev)) + geom_jitter()
 unique(test$year)
