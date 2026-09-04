3a9edfd4a10311dcb9a718e79a8439db80acc948
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-09-04 13:40:52 -0700

    rename

--- analysis/calibration/01-calibration-coeff-paired-comp-data.R
+++ analysis/calibration/01-calibration-coeff-paired-comp-data.R
@@ -1,3 +1,5 @@
+#calibration coeff for paired comparative sets, tested calibration coeff by length, depth, and intercept only model
+
 library(dplyr)
 library(tidyr)
 library(sdmTMB)
@@ -34,9 +36,6 @@ test2 <- test |> group_by(length_bin, hook_name, fishing_event_id, year) |>
   drop_na(length_bin) |>
   ungroup()
 
-#df_dog <- filter(test2, hook_name == "Dog_14/0")
-#df_hbll <- filter(test2, hook_name == "HBLL_13/0")
-
 test3 <- test |> left_join(test2) |> dplyr::select(-length, -sex) |> distinct()
 
 test4 <- test3 |>
@@ -66,20 +65,6 @@ depth <- samps |> group_by(hook_name, fishing_event_id, year, grouping_depth_id)
   rename(catch_count_HBLL = "catch_count_HBLL_13/0" , catch_count_Dog = "catch_count_Dog_14/0") |>
   mutate(offset = log(offset_HBLL) - log(offset_Dog))
 
-# test_all <- samps |> group_by(hook_name, fishing_event_id, year) |>
-#   reframe(catch_count = n(), offset = mean(lglsp_hook_count)) |>
-#   ungroup() |>
-#   pivot_wider(
-#     names_from = hook_name    ,
-#     values_from = c(catch_count, offset)
-#   ) |>
-#   ungroup() |>
-#   arrange(year, fishing_event_id) |>
-#   drop_na("offset_HBLL_13/0", "offset_Dog_14/0") |>
-#   rename(offset_HBLL = "offset_HBLL_13/0" , offset_Dog = "offset_Dog_14/0") |>
-#   rename(catch_count_HBLL = "catch_count_HBLL_13/0" , catch_count_Dog = "catch_count_Dog_14/0") |>
-#   mutate(offset = log(offset_HBLL) - log(offset_Dog))
-
 test5 <- test3 |>
   select(year, fishing_event_id, lglsp_hook_count, hook_name, grouping_depth_id) %>%
   distinct() |>
@@ -106,10 +91,9 @@ final <- add_utm_columns(
   utm_names = c("UTM.lat", "UTM.lon"))
 
 
-#model run for length based calibration coefficient
-dummy_mesh <- sdmTMB::make_mesh(final, c("UTM.lon", "UTM.lat"), n_knots = 10)
+# length based model ------------------------------------------------------
 
-#final <- final |> mutate(cpue_hbll_length = catch_count_hbll_length/exp(offset), cpue_dog_length = catch_count_dog_length/exp(offset))
+dummy_mesh <- sdmTMB::make_mesh(final, c("UTM.lon", "UTM.lat"), n_knots = 10)
 final <- final |> drop_na()
 weight = exp(final$offset)
 dummy_mesh <- sdmTMB::make_mesh(final, c("UTM.lon", "UTM.lat"), n_knots = 10)
@@ -134,15 +118,10 @@ exp(tidy(m2)$estimate)
 exp(tidy(m2)$conf.low)
 exp(tidy(m2)$conf.high)
 
-q1 <- coef(m2)["factor(length_bin)(43,64]"]
-exp(q1) #1.74 for smallest length bin
-q2 <- coef(m2)["factor(length_bin)(64,84]"]
-exp(q2)
-q3 <- coef(m2)["factor(length_bin)(84,113]"]
-exp(q3)
 
 
-#no length
+# intercept only model ----------------------------------------------------
+
 weight = exp(depth$offset)
 
 all <- sdmTMB(
@@ -151,8 +130,6 @@ all <- sdmTMB(
   spatial = "off",
   spatiotemporal = "off",
   data = depth,
-  #offset = final$offset,
-  #family = binomial(),
   family = betabinomial(),
   weights = weight,
   control = sdmTMBcontrol(multiphase = FALSE)
@@ -165,6 +142,8 @@ exp(all2$conf.low)
 exp(all2$conf.high)
 
 
+
+# depth model -------------------------------------------------------------
 
 weight = exp(depth$offset)
 
@@ -187,6 +166,9 @@ exp(depthc$conf.low)
 exp(depthc$conf.high)
 
 
+
+# random effect of site model ---------------------------------------------
+
 site <- sdmTMB(
   cbind(catch_count_HBLL , catch_count_Dog ) ~ 1,
   #cbind(cpue_hbll , cpue_dog ) ~ 1 + (1|fishing_event_id),
@@ -208,41 +190,17 @@ exp(site2$conf.low)
 exp(site2$conf.high)
 
 
+
+# compete aics ------------------------------------------------------------
+
 AIC(all)
 AIC(m2)
 AIC(depthm)
 
 table = (tidy(all))
+
 exp(table$estimate)
 exp(table$conf.low)
 exp(table$conf.high)
-
-#log_offset = log(mean(c(exp(q1), exp(q2), exp(q3))))
-#log_offset
-#exp(log_offset) #68 percent more fish caught on HBLL
-
-length <- data.frame("Length (cm)" = c("43-64", "64-84", "84-113", "all data"))
-q <- data.frame("rho" = round(c(exp(q1), exp(q2), exp(q3), exp(all2$estimate)), 2))
-Data <- data.frame(Data = "paired")
-table <- cbind(Data, length, q)
-table$conf.low = c(round(exp(tidy(m2)$conf.low),2), round(exp(all2$conf.low),2))
-table$conf.high = c(round(exp(tidy(m2)$conf.high),2), round(exp(all2$conf.high),2))
-table$CI <- paste0((table$conf.low), "-", (table$conf.high))
-table <- table |> mutate(final = paste0(rho, " (", conf.low, "-", conf.high, ")"))
-table <- table |> dplyr::select("Data", "Length..cm.", final )
-rownames(table) <- NULL
-
-table |>
-  knitr::kable(
-    format = "latex",
-    col.names = c("Data",
-                  "Length bin (cm)", "rho (CI)"),
-    booktabs = TRUE,
-    align = "c",
-    caption = "rho for seasonally paired comparative sets",
-    label = "length_calibration_season"
-  ) |>
-  #kableExtra::row_spec(0, bold = TRUE, color = "white", background = "grey")  |>  # header styling
-  kableExtra::kable_styling(full_width = FALSE)
 
 
