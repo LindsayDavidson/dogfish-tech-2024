eb0ea71efcfdbbdeac5d8f796ba707b9e2d359eb
Davidson <Lindsay.Davidson@dfo-mpo.gc.ca>
2026-09-04 13:34:41 -0700

    sensitivity figure

--- analysis/index/99-composite-index-figure.R
+++ analysis/index/99-composite-index-figure.R
@@ -1,33 +1,51 @@
-write_csv(index_dog, file = "analysis/calibration/index/index_dog.csv")
-#write_csv(index_dog, file = "analysis/calibration/index/index_dog_julian.csv")
-write_csv(index_hbll, file = "analysis/calibration/index/index_hbll.csv")
-write_csv(index_dc, file = "analysis/calibration/index/index_dogfish_jcirclehook_calibrate.csv")
-write_csv(index, file = "analysis/calibration/index/index_dogfish_calibrate.csv")
+#figure of calibrated indicies
+
+#index_dog <- read_csv(file = "data-generated/index_dog.csv")
+index_hbll <- read_csv(file = "data-generated/index_hbll.csv")
+index_dc <- read_csv(file = "data-generated/index_dogfish_jcirclehook_calibrate.csv")
+index <- read_csv(file = "data-generated/index_dogfish_calibrate.csv")
+index_mean <- read_csv(file = "data-generated/index_dogfish_jcirclehookhbll_mean.csv")
+
+
+# sensitivity to calibration params ---------------------------------------
+
+gg <-
+  index %>%
+  ggplot(aes(year, est, ymin = lwr, ymax = upr, group = iter)) +
+  geom_ribbon(aes(year, est, ymin = lwr, ymax = upr ), fill = "grey90", colour  = NA, alpha = 0.05) +
+  geom_line(aes(group = iter), colour = "grey70", linewidth = 1, alpha = 0.15) +
+  geom_point(aes(group = iter), colour = "grey70", size = 2, alpha = 0.15) +
+  #geom_linerange() +
+  #facet_wrap(vars(Survey), ncol = 1, scales = "free_y") +
+  expand_limits(y = 0) +
+  guides(fill = "none", colour = "none") +
+  #scale_colour_viridis_d() +
+  #scale_fill_viridis_d() +
+  labs(x = "Year", y = "Index") +
+  theme_classic()
+gg2 <- gg +
+  geom_ribbon(data = index_mean, aes(year, est, ymin = lwr, ymax = upr), fill = "red", alpha = 0.05) +
+  geom_line(data = index_mean, aes(year, est), colour = "red") +
+  geom_point(data = index_mean, aes(year, est), colour = "red")
+
+ggsave("figures/sensitivity_calibrated_index.jpg", gg2, width = 4, height =4)
+
 
 
 
 # compare indices ---------------------------------------------------------
 
-
 index_compare <- rbind(   #seasonally paired value and center data
-  index |> mutate(Survey = "Calibrated HBLL + SoG dogfish") |>
+  index |> mutate(Survey = "Calibrated HBLL & SoG dogfish") |>
     mutate(est_c = scale(est, center = TRUE, scale = TRUE), lwr_c = (lwr - mean(est))/sd(est), upr_c = (upr - mean(est))/sd(est)),
   index_hbll |> mutate(Survey = "HBLL") |>
     mutate(est_c = scale(est, center = TRUE, scale = TRUE), lwr_c = (lwr - mean(est))/sd(est), upr_c = (upr - mean(est))/sd(est)),
   #index_dog |> mutate(Survey = "SoG dogfish (circle)") |>
   #  mutate(est_c = scale(est, center = TRUE, scale = TRUE), lwr_c = (lwr - mean(est))/sd(est), upr_c = (upr - mean(est))/sd(est)),
-  index_dc |> mutate(Survey = "Calibrated SoG dogfish") |>
+  index_dc |> mutate(Survey = "Calibrated SoG dogfish circle & Jhook") |>
     mutate(est_c = scale(est, center = TRUE, scale = TRUE), lwr_c = (lwr - mean(est))/sd(est), upr_c = (upr - mean(est))/sd(est))
 
 )
-
-# index_compare2 <- index_compare <- rbind(
-#   index |> mutate(Survey = "Calibrated HBLL + SoG dogfish (circle and j-hook)", value = "1.19"), #paired value
-#   index_hbll |> mutate(Survey = "HBLL", value = "1.19"),
-#   index_dog |> mutate(Survey = "SoG dogfish", value = "1.19")
-# )
-
-#index_compare <- rbind(index_compare, index_compare2)
 
 x <- palette.colors(palette = "Okabe-Ito")
 
@@ -48,26 +66,3 @@ gg <- index_compare %>%
 
 ggsave("figures/calibrated_index.jpg", gg, width = 6, height =3)
 
-# index_compare <- rbind(
-#   read.csv(file = "analysis/calibration/index/index_dogfish_hbll_calibrate.csv") %>%
-#     mutate(Survey = "Calibrated HBLL + SoG dogfish"),
-#   read.csv(file = "analysis/calibration/index/index_dog.csv") %>%
-#     mutate(Survey = "SoG dogfish"),
-#   read.csv(file = "analysis/calibration/index/index_hbll.csv") %>%
-#     mutate(Survey = "HBLL")
-# )
-#
-# year_dogfish <- index_compare %>%
-#   filter(Survey == "SoG dogfish") %>%
-#   pull(year)
-#
-# gg <- index_compare %>%
-#   mutate(dyear = year %in% year_dogfish) %>%
-#   ggplot(aes(year, est, ymin = lwr, ymax = upr, colour = dyear)) +
-#   geom_point() +
-#   geom_line(aes(group = Survey), linewidth = 0.1) +
-#   geom_linerange() +
-#   facet_wrap(vars(Survey), ncol = 1, scales = "free_y") +
-#   expand_limits(y = 0) +
-#   labs(x = "Year", y = "Index", colour = "Year with \nSoG dogfish survey?")
-# ggsave("analysis/index-calibration/index/compare_index.png", g, height = 6, width = 5)
